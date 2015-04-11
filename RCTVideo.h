ef481932663ac51b026978011f66c859b7706782
Mike Wilcox <mwilcox56@gmail.com>
2015-04-10 22:57:37 -0400

    add RNVideoEventEnd constant

--- RCTVideo.h
+++ RCTVideo.h
@@ -5,6 +5,7 @@ extern NSString *const RNVideoEventLoading;
 extern NSString *const RNVideoEventProgress;
 extern NSString *const RNVideoEventSeek;
 extern NSString *const RNVideoEventLoadingError;
+extern NSString *const RNVideoEventEnd;
 
 @class RCTEventDispatcher;
 
