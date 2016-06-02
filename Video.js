c8833f185e67336ac36b4a94dec1c55cdbcfa382
huangmr <huangmr@hotmail.com>
2016-06-02 16:25:19 +0100

    integrate ExoPlayer on android

--- Video.js
+++ Video.js
@@ -160,7 +160,7 @@ export default class Video extends Component {
     }
 
     const isNetwork = !!(uri && uri.match(/^https?:/));
-    const isAsset = !!(uri && uri.match(/^(assets-library|file|content):/));
+    const isAsset = !!(uri && uri.match(/^(assets-library|file|content|asset):/));
 
     let nativeResizeMode;
     if (resizeMode === VideoResizeMode.stretch) {
