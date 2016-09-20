7a1a0a82d00c3204d4bf8ff63486d79a0f66c3f0
Evgeniy Sokovikov <skv-headless@yandex.ru>
2016-09-20 04:46:15 +0400

    udgrate example to rn33 (#329)

---
+++ Examples/VideoPlayer/android/app/src/main/java/com/videoplayer/MainApplication.java
@@ -0,0 +1,37 @@
+package com.videoplayer;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new ReactVideoPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+      return mReactNativeHost;
+  }
+}
