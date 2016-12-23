df7ddc619f6a89976c02c55d5bbceae72f00c5a4
Dmitriy Loktev <unknownliveid@hotmail.com>
2016-12-23 18:43:33 +0300

    Getting ready for RN@0.40

--- ios/RCTVideo.h
+++ ios/RCTVideo.h
@@ -1,7 +1,7 @@
-#import "RCTView.h"
+#import "React/RCTView.h"
 #import <AVFoundation/AVFoundation.h>
 #import "AVKit/AVKit.h"
-#import "UIView+FindUIViewController.h"
+#import "React/UIView+FindUIViewController.h"
 #import "RCTVideoPlayerViewController.h"
 #import "RCTVideoPlayerViewControllerDelegate.h"
 
