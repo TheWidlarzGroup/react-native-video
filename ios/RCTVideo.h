a0ba525e429bcea9442c8b45b0a1f4fbcf1bdda4
Dmitriy Loktev <unknownliveid@hotmail.com>
2016-12-26 12:46:43 +0300

    Fix header

--- ios/RCTVideo.h
+++ ios/RCTVideo.h
@@ -1,7 +1,7 @@
-#import "React/RCTView.h"
+#import <React/RCTView.h>
 #import <AVFoundation/AVFoundation.h>
 #import "AVKit/AVKit.h"
-#import "React/UIView+FindUIViewController.h"
+#import "UIView+FindUIViewController.h"
 #import "RCTVideoPlayerViewController.h"
 #import "RCTVideoPlayerViewControllerDelegate.h"
 
