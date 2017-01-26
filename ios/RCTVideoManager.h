1a3520ec62c80349468b0f241fbfbd7e0ada52f8
Enguerrand des Vaux <enguerrand.desvaux@gmail.com>
2017-01-26 17:30:47 +1000

    Update RCTVideoManager.h

--- ios/RCTVideoManager.h
+++ ios/RCTVideoManager.h
@@ -1,4 +1,11 @@
+// import RCTViewManager
+#if __has_include(<React/RCTViewManager.h>)
 #import <React/RCTViewManager.h>
+#elif __has_include("RCTViewManager.h")
+#import "RCTViewManager.h"
+#else
+#import "React/RCTViewManager.h"   // Required when used as a Pod in a Swift project
+#endif
 
 @interface RCTVideoManager : RCTViewManager
 
