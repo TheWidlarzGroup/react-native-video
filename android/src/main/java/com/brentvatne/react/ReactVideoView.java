ccb583ffcfe62f68bed484b05af305701c970cfb
Deon Nicholas <dojiboy9@hotmail.com>
2017-03-17 10:47:17 -0700

    Fix VideoView on Android

--- android/src/main/java/com/brentvatne/react/ReactVideoView.java
+++ android/src/main/java/com/brentvatne/react/ReactVideoView.java
@@ -294,9 +294,8 @@ public class ReactVideoView extends ScalableVideoView implements MediaPlayer.OnP
         event.putMap(ReactVideoViewManager.PROP_SRC, src);
         mEventEmitter.receiveEvent(getId(), Events.EVENT_LOAD_START.toString(), event);
 
-        // not async to prevent random crashes on Android playback from local resource due to race conditions
         try {
-          prepare(this);
+          prepareAsync(this);
         } catch (Exception e) {
           e.printStackTrace();
         }
