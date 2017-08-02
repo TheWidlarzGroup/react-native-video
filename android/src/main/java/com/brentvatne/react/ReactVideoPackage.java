78f368c64e04c36b15c2660e2d72b18d931ec342
Radek Czemerys <radko93@gmail.com>
2017-08-02 19:41:35 +0200

    Added comment

--- android/src/main/java/com/brentvatne/react/ReactVideoPackage.java
+++ android/src/main/java/com/brentvatne/react/ReactVideoPackage.java
@@ -18,6 +18,7 @@ public class ReactVideoPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
