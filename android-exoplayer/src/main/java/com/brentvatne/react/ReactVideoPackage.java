ac8036cecb72d877229fec49946c3f0a96b71dc7
Radek Czemerys <radko93@gmail.com>
2017-08-02 23:40:14 +0200

    Fix typo in comment

--- android-exoplayer/src/main/java/com/brentvatne/react/ReactVideoPackage.java
+++ android-exoplayer/src/main/java/com/brentvatne/react/ReactVideoPackage.java
@@ -17,7 +17,7 @@ public class ReactVideoPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
-    // Deprecated RN 0.47
+    // Depreciated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
