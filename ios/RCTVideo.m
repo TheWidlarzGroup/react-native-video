df8f3d8bcd635b8de490dcfe1999defb9acf4b01
takushi ueda <takushi.ueda@gmail.com>
2018-06-05 11:30:13 +0900

    Revert "add optional variable to setCurrentTime method"
    This reverts commit 327a25bef8ec8c877df942381e55d9ebde4dfea4.

--- ios/RCTVideo.m
+++ ios/RCTVideo.m
@@ -554,20 +554,13 @@ static NSString *const timedMetadata = @"timedMetadata";
 
 - (void)setCurrentTime:(float)currentTime
 {
-
-  [self setSeek:currentTime toleranceInMS:1000];
-
+  [self setSeek: currentTime];
 }
 
-- (void)setCurrentTime:(float)currentTime toleranceInMS:(int)toleranceInMS
+- (void)setSeek:(float)seekTime
 {
-  [self setSeek:currentTime toleranceInMS:toleranceInMS];
+  int timeScale = 10000;
 
-}
-
-- (void)setSeek:(float)seekTime toleranceMS:(int)toleranceMS
-{
-  int timeScale = 1000;
   AVPlayerItem *item = _player.currentItem;
   if (item && item.status == AVPlayerItemStatusReadyToPlay) {
     // TODO check loadedTimeRanges
@@ -575,11 +568,11 @@ static NSString *const timedMetadata = @"timedMetadata";
     CMTime cmSeekTime = CMTimeMakeWithSeconds(seekTime, timeScale);
     CMTime current = item.currentTime;
     // TODO figure out a good tolerance level
-    CMTime tolerance = CMTimeMake(toleranceMS, timeScale);
     BOOL wasPaused = _paused;
+
     if (CMTimeCompare(current, cmSeekTime) != 0) {
       if (!wasPaused) [_player pause];
-      [_player seekToTime:cmSeekTime toleranceBefore:tolerance toleranceAfter:tolerance completionHandler:^(BOOL finished) {
+      [_player seekToTime:cmSeekTime toleranceBefore:kCMTimeZero toleranceAfter:kCMTimeZero completionHandler:^(BOOL finished) {
         if (!_timeObserver) {
           [self addPlayerTimeObserver];
         }
@@ -587,11 +580,12 @@ static NSString *const timedMetadata = @"timedMetadata";
           [self setPaused:false];
         }
         if(self.onVideoSeek) {
-          self.onVideoSeek(@{@"currentTime": [NSNumber numberWithFloat:CMTimeGetSeconds(item.currentTime)],
-                             @"seekTime": [NSNumber numberWithFloat:seekTime],
-                             @"target": self.reactTag});
+            self.onVideoSeek(@{@"currentTime": [NSNumber numberWithFloat:CMTimeGetSeconds(item.currentTime)],
+                               @"seekTime": [NSNumber numberWithFloat:seekTime],
+                               @"target": self.reactTag});
         }
       }];
+
       _pendingSeek = false;
     }
 
