5a053a74694c5e0a0f1dde5119a88379ebfcfe93
Hampton Maxwell <me@hamptonmaxwell.com>
2018-12-13 10:05:09 -0800

    Fix bug where useTextureView wasn't defaulting to true

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -131,8 +131,6 @@ class ReactExoplayerView extends FrameLayout implements
     private boolean disableFocus;
     private float mProgressUpdateInterval = 250.0f;
     private boolean playInBackground = false;
-    private boolean useTextureView = false;
-    private boolean hideShutterView = false;
     private Map<String, String> requestHeaders;
     // \ End props
 
