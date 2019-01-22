48a0de66e9f0d9f7ec25ed25f9ba8b5982e330f4
n1ru4l <laurinquast@googlemail.com>
2019-01-22 11:43:14 +0100

    chore: update example app

--- examples/video-caching/rn-cli.config.js
+++ examples/video-caching/rn-cli.config.js
@@ -1,7 +1,14 @@
+"use strict";
+
+const path = require('path');
 const blacklist = require('metro').createBlacklist;
 
 module.exports = {
+  // Resolve react-native-video from parent directory so we do not have to install react-native-video after each change applied
   getBlacklistRE: function() {
-    return blacklist([/node_modules\/react-native-video\/examples\/.*/]);
-  }
+    return blacklist([/node_modules\/react-native-video\/.*/,  /\/Users\/laurin\/projects\/react-native-video\/node_modules\/react-native\/.*/]);
+  },
+  getProjectRoots() {
+    return [__dirname, path.resolve(__dirname, "..", "..")];
+  },
 };
