cdfb6305abf2d42c035a46d87a3b23e5faa17f17
Daniel Mariño <daniel.marino@24imedia.com>
2019-01-23 10:39:39 +0100

    unneeded toast

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.Window;
 import android.view.accessibility.CaptioningManager;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.brentvatne.react.R;
 import com.brentvatne.receiver.AudioBecomingNoisyReceiver;
