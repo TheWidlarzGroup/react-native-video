a89ac0c76f1a223ac6cdab03c7957e96c3731815
Daniel Mariño <daniel.marino@24imedia.com>
2019-01-23 16:44:30 +0100

    clean base64 + native method

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -1739,151 +1739,17 @@ static int const RCTVideoUnset = -1;
     return true;
 }
 
-- (NSData *)base64DataFromString: (NSString *)string
-{
-    unsigned long ixtext, lentext;
-    unsigned char ch, inbuf[4], outbuf[3];
-    short i, ixinbuf;
-    Boolean flignore, flendtext = false;
-    const unsigned char *tempcstring;
-    NSMutableData *theData;
+- (NSData *)base64DataFromString: (NSString *)string {
+    // Create NSData object
+    NSData *nsdata = [string
+                      dataUsingEncoding:NSUTF8StringEncoding];
+    // Get NSString from NSData object in Base64
+    NSString *base64Encoded = [nsdata base64EncodedStringWithOptions:0];
     
-    if (string == nil)
-    {
-        return [NSData data];
-    }
-    
-    ixtext = 0;
-    
-    tempcstring = (const unsigned char *)[string UTF8String];
-    
-    lentext = [string length];
-    
-    theData = [NSMutableData dataWithCapacity: lentext];
-    
-    ixinbuf = 0;
-    
-    while (true)
-    {
-        if (ixtext >= lentext)
-        {
-            break;
-        }
-        
-        ch = tempcstring [ixtext++];
-        
-        flignore = false;
-        
-        if ((ch >= 'A') && (ch <= 'Z'))
-        {
-            ch = ch - 'A';
-        }
-        else if ((ch >= 'a') && (ch <= 'z'))
-        {
-            ch = ch - 'a' + 26;
-        }
-        else if ((ch >= '0') && (ch <= '9'))
-        {
-            ch = ch - '0' + 52;
-        }
-        else if (ch == '+')
-        {
-            ch = 62;
-        }
-        else if (ch == '=')
-        {
-            flendtext = true;
-        }
-        else if (ch == '/')
-        {
-            ch = 63;
-        }
-        else
-        {
-            flignore = true;
-        }
-        
-        if (!flignore)
-        {
-            short ctcharsinbuf = 3;
-            Boolean flbreak = false;
-            
-            if (flendtext)
-            {
-                if (ixinbuf == 0)
-                {
-                    break;
-                }
-                
-                if ((ixinbuf == 1) || (ixinbuf == 2))
-                {
-                    ctcharsinbuf = 1;
-                }
-                else
-                {
-                    ctcharsinbuf = 2;
-                }
-                
-                ixinbuf = 3;
-                
-                flbreak = true;
-            }
-            
-            inbuf [ixinbuf++] = ch;
-            
-            if (ixinbuf == 4)
-            {
-                ixinbuf = 0;
-                
-                outbuf[0] = (inbuf[0] << 2) | ((inbuf[1] & 0x30) >> 4);
-                outbuf[1] = ((inbuf[1] & 0x0F) << 4) | ((inbuf[2] & 0x3C) >> 2);
-                outbuf[2] = ((inbuf[2] & 0x03) << 6) | (inbuf[3] & 0x3F);
-                
-                for (i = 0; i < ctcharsinbuf; i++)
-                {
-                    [theData appendBytes: &outbuf[i] length: 1];
-                }
-            }
-            
-            if (flbreak)
-            {
-                break;
-            }
-        }
-    }
-    
-    return theData;
-}
-
-- (NSString*)base64forData:(NSData*)theData {
-    const uint8_t* input = (const uint8_t*)[theData bytes];
-    NSInteger length = [theData length];
-    
-    static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    
-    NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
-    uint8_t* output = (uint8_t*)data.mutableBytes;
-    
-    NSInteger i;
-    for (i=0; i < length; i += 3) {
-        NSInteger value = 0;
-        NSInteger j;
-        for (j = i; j < (i + 3); j++) {
-            value <<= 8;
-            
-            if (j < length) {
-                value |= (0xFF & input[j]);
-            }
-        }
-        
-        NSInteger theIndex = (i / 3) * 4;
-        output[theIndex + 0] =                    table[(value >> 18) & 0x3F];
-        output[theIndex + 1] =                    table[(value >> 12) & 0x3F];
-        output[theIndex + 2] = (i + 1) < length ? table[(value >> 6)  & 0x3F] : '=';
-        output[theIndex + 3] = (i + 2) < length ? table[(value >> 0)  & 0x3F] : '=';
-    }
-    
-    return [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+    // NSData from the Base64 encoded str
+    NSData *nsdataFromBase64String = [[NSData alloc]
+                                      initWithBase64EncodedString:base64Encoded options:0];
+    return nsdataFromBase64String;
 }
 
 @end
