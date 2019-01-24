194d30cab056206b83898d88ab42ab6023196cd4
n1ru4l <laurinquast@googlemail.com>
2019-01-24 13:13:03 +0100

    fix: remove user specific configuration

--- examples/video-caching/rn-cli.config.js
+++ examples/video-caching/rn-cli.config.js
@@ -3,12 +3,17 @@
 const path = require('path');
 const blacklist = require('metro').createBlacklist;
 
+const rootProjectDir = path.resolve(__dirname, '..', '..')
+
 module.exports = {
   // Resolve react-native-video from parent directory so we do not have to install react-native-video after each change applied
   getBlacklistRE: function() {
-    return blacklist([/node_modules\/react-native-video\/.*/,  /\/Users\/laurin\/projects\/react-native-video\/node_modules\/react-native\/.*/]);
+    return blacklist([/node_modules\/react-native-video\/.*/, new RegExp(`${rootProjectDir}/node_modules/react-native/.*`)])
   },
   getProjectRoots() {
-    return [__dirname, path.resolve(__dirname, "..", "..")];
-  },
+    return [
+      __dirname, 
+      rootProjectDir
+    ]
+  }
 };
