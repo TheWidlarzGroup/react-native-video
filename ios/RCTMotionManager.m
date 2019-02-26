d572325c4e15e131b395eee788022fcd64e6e6a1
Chen Li <chen@loopapps.com>
2019-02-25 17:06:36 -0800

    Bending

--- ios/RCTMotionManager.m
+++ ios/RCTMotionManager.m
@@ -11,12 +11,10 @@
 #import <math.h>
 #import "OvalCalculator.h"
 
-static double const kMaxLockAngle = -0.209;  // 12 degree, clockwise
-static double const kMinLockAngle = -6.074;   // 12 degree, counter-clockwise
-
 typedef enum {
   RCTMotionManagerStateFree,      // Free to track device rotation
-  RCTMotionManagerStateLocked,    // Locked to certain angles
+  RCTMotionManagerStateLocked,    // May free move within 15 degrees, and bend within 20 degrees
+  RCTMotionManagerStateBouncing,  // During a bouncing animation
   RCTMotionManagerStateUnlocking  // During an unlock animation
 } RCTMotionManagerState;
 
@@ -50,7 +48,8 @@ typedef enum {
   return self;
 }
 
-- (CGAffineTransform) transformWithRotation: (CGFloat) rotation {
+- (CGAffineTransform)transformWithRotation:(CGFloat)rotation
+                                translateX:(CGFloat)translateX {
   [_scaler set_fit];
   double scale =
   [_scaler get_scaleWithDouble:_viewWidth
@@ -59,7 +58,8 @@ typedef enum {
                     withDouble:_videoHeight
                     withDouble:rotation];
   CGAffineTransform transform = CGAffineTransformMakeScale(scale, scale);
-  return CGAffineTransformRotate(transform, rotation);
+  transform = CGAffineTransformRotate(transform, rotation);
+  return CGAffineTransformTranslate(transform, translateX, 0.0);
 }
 
 - (BOOL) isFlatWithGravity:(CMAcceleration) gravity {
@@ -76,8 +76,9 @@ typedef enum {
 
     __strong RCTMotionManager *strongSelf = weakSelf;
     if (strongSelf == nil) { return; }
-    if (strongSelf->_lockState == RCTMotionManagerStateUnlocking) {
-      // Unlocking animation is going on, don't use sensor's input
+    if (strongSelf->_lockState == RCTMotionManagerStateUnlocking
+        || strongSelf->_lockState == RCTMotionManagerStateBouncing) {
+      // An animation is going on, don't use sensor's input
       return;
     }
     if (motion == nil) { return; }
@@ -91,19 +92,28 @@ typedef enum {
     lastY = gravity.y * decay + lastY * (1 - decay);
 
     double rawRotation = atan2(lastX, lastY) - M_PI;
-    double rotation = [strongSelf.class rotationWithLockState:_lockState rawRotation:rawRotation];
+    if (rawRotation < -3.142) {
+      rawRotation = rawRotation + 6.283;
+    }
+    BOOL shouldBounce = NO;
+    double translateX = 0.0;
+    double rotation = [strongSelf.class rotationWithLockState:_lockState
+                                                  rawRotation:rawRotation
+                                                   translateX:&translateX
+                                                 shouldBounce:&shouldBounce];
     _initialRotationWhenUnlocking = rotation;
     _rotationDeltaForUnlocking = rawRotation - rotation;
     //    printf("rawRotation: %.2f, rotation: %.2f\n", rawRotation, rotation);
 
     if (handler) {
-      handler([strongSelf transformWithRotation:rotation]);
+      handler([strongSelf transformWithRotation:rotation
+                                     translateX:translateX]);
     }
   }];
 }
 
 - (CGAffineTransform)getZeroRotationTransform {
-  return [self transformWithRotation:0];
+  return [self transformWithRotation:0 translateX:0.0];
 }
 
 - (void)stopDeviceMotionUpdates {
@@ -112,16 +122,36 @@ typedef enum {
 
 #pragma mark - Time Lock
 
-+ (double)rotationWithLockState:(RCTMotionManagerState)lockState rawRotation:(double)rawRotaton {
-  static double midLockAngle = (kMinLockAngle + kMaxLockAngle) / 2.0;
-  if (lockState == RCTMotionManagerStateLocked) {
-    if (rawRotaton > kMinLockAngle && rawRotaton <= midLockAngle) {
-      return kMinLockAngle;
-    } else if (rawRotaton > midLockAngle && rawRotaton < kMaxLockAngle) {
-      return kMaxLockAngle;
-    }
++ (double)rotationWithLockState:(RCTMotionManagerState)lockState
+                    rawRotation:(double)rawRotation
+                     translateX:(double *)translateX
+                   shouldBounce:(BOOL *)shouldBounce {
+  *shouldBounce = NO;
+  *translateX = 0.0;
+
+  if (lockState == RCTMotionManagerStateFree) {
+    return rawRotation;
   }
-  return rawRotaton;
+
+  assert(lockState == RCTMotionManagerStateLocked);
+  double const sign = (rawRotation >= 0.0) ? 1.0 : -1.0;
+  rawRotation = rawRotation * sign;
+
+  static double const kLockAngle = 0.262;       // 15 degrees
+  static double const kBounceAngle = 0.349;  // 20 degrees
+  static double const kMaxTranslateX = 24.0;    // In points
+
+  if (rawRotation > kLockAngle && rawRotation <= kBounceAngle) {
+    // Bending
+    *translateX = -sign * kMaxTranslateX * (rawRotation - kLockAngle) / (kBounceAngle - kLockAngle);
+    rawRotation = kLockAngle;
+  } else if (rawRotation > kBounceAngle) {
+    // Bouncing
+    *translateX = -sign * kMaxTranslateX;
+    rawRotation = kLockAngle;
+    *shouldBounce = YES;
+  }
+  return rawRotation * sign;
 }
 
 - (void)lock {
@@ -143,7 +173,7 @@ typedef enum {
   //  printf("sampleAnimator timeElapsed: %.2f, factor: %.2f\n", timeElapsed, factor);
   double rotation = _initialRotationWhenUnlocking + _rotationDeltaForUnlocking * factor;
   if (_updatesHandler) {
-    _updatesHandler([self transformWithRotation:rotation]);
+    _updatesHandler([self transformWithRotation:rotation translateX:0.0]);
   }
 
   if (timeElapsed > 1.4) {
@@ -157,7 +187,7 @@ typedef enum {
  we had to use a spring animation equation from
  https://medium.com/@dtinth/spring-animation-in-css-2039de6e1a03
  f(0) = 0; f'(0) = 0; f''(t) = -100(f(t) - 1) - 16f'(t)
-*/
+ */
 - (double)springAnimationFactorWithTimeElapsed:(CFTimeInterval)timeElapsed {
   double const coefficientTime = 6.0 * timeElapsed;
   double const exponential = exp2(-8.0*timeElapsed);
