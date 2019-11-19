a5bfba9451244c095d72a62fda0e0fb8d7d08c84
Ash Mishra <a.mishra@nfb.ca>
2019-11-19 12:32:11 -0800

    Fixes a situation when exiting fullscreen, where observed keypath values have not been released

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -1381,6 +1381,8 @@ static int const RCTVideoUnset = -1;
 {
   if (_playerViewController == playerViewController && _fullscreenPlayerPresented && self.onVideoFullscreenPlayerWillDismiss)
   {
+    [_playerViewController.contentOverlayView removeObserver:self forKeyPath:@"frame"];
+    [_playerViewController removeObserver:self forKeyPath:readyForDisplayKeyPath];
     self.onVideoFullscreenPlayerWillDismiss(@{@"target": self.reactTag});
   }
 }
