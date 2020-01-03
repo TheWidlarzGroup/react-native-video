a44ff111916e2eb58d16e3df13f1b46999ecabb4
Tuan Luong <tuanluong.it@gmail.com>
2020-01-03 09:35:40 +0700

    update UIInterfaceOrientation in fullscreen

--- ios/Video/RCTVideoPlayerViewController.m
+++ ios/Video/RCTVideoPlayerViewController.m
@@ -28,7 +28,8 @@
 
 - (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
   if ([self.preferredOrientation.lowercaseString isEqualToString:@"landscape"]) {
-    return UIInterfaceOrientationLandscapeRight;
+    UIDeviceOrientation currentOrientation = [[UIDevice currentDevice] orientation];
+    return currentOrientation == UIDeviceOrientationLandscapeRight ? UIInterfaceOrientationLandscapeLeft : UIInterfaceOrientationLandscapeRight;
   }
   else if ([self.preferredOrientation.lowercaseString isEqualToString:@"portrait"]) {
     return UIInterfaceOrientationPortrait;
