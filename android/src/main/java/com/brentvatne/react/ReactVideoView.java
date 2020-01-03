6fa41b1c15dc8c864dd4916e01bfe50fe889c0b8
Tarık İNCE <incetarik@hotmail.com>
2020-01-03 11:07:04 +0300

    Added activity orientation change for fullscreen
    Added activity orientation change for the full screen by using `setRequestOrientation` function of `activity` to set landscape orientation when both fullscreen property and `requestOrientationInFullscreen` property set to true.
    It can be used for automatically rotating the application when the video is in full screen like other applications.

--- android/src/main/java/com/brentvatne/react/ReactVideoView.java
+++ android/src/main/java/com/brentvatne/react/ReactVideoView.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.Window;
 import android.webkit.CookieManager;
 import android.widget.MediaController;
+import android.content.pm.ActivityInfo;
 
 import com.android.vending.expansion.zipfile.APKExpansionSupport;
 import com.android.vending.expansion.zipfile.ZipResourceFile;
@@ -132,6 +133,7 @@ public class ReactVideoView extends ScalableVideoView implements
     private boolean mPlayInBackground = false;
     private boolean mBackgroundPaused = false;
     private boolean mIsFullscreen = false;
+    private boolean mRequestOrientationInFullscreen = false;
 
     private int mMainVer = 0;
     private int mPatchVer = 0;
@@ -478,6 +480,10 @@ public class ReactVideoView extends ScalableVideoView implements
             }
         }
     }
+        
+    public void setRequestOrientationInFullscreen(boolean requestOrientationInFullscreen) {
+        mRequestOrientationInFullscreen = requestOrientationInFullscreen;
+    }
 
     public void setFullscreen(boolean isFullscreen) {
         if (isFullscreen == mIsFullscreen) {
@@ -503,11 +509,21 @@ public class ReactVideoView extends ScalableVideoView implements
             }
             mEventEmitter.receiveEvent(getId(), Events.EVENT_FULLSCREEN_WILL_PRESENT.toString(), null);
             decorView.setSystemUiVisibility(uiOptions);
+            
+            if (mRequestOrientationInFullscreen) {
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
+            
             mEventEmitter.receiveEvent(getId(), Events.EVENT_FULLSCREEN_DID_PRESENT.toString(), null);
         } else {
             uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
             mEventEmitter.receiveEvent(getId(), Events.EVENT_FULLSCREEN_WILL_DISMISS.toString(), null);
             decorView.setSystemUiVisibility(uiOptions);
+            
+            if (mRequestOrientationInFullscreen) {
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+            
             mEventEmitter.receiveEvent(getId(), Events.EVENT_FULLSCREEN_DID_DISMISS.toString(), null);
         }
     }
@@ -522,7 +538,6 @@ public class ReactVideoView extends ScalableVideoView implements
     }
 
     public void setPlayInBackground(final boolean playInBackground) {
-
         mPlayInBackground = playInBackground;
     }
 
