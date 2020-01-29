fc5aa96d910fd208954e88011f850265e89a1d43
Hamdi Hakan Karakaya <hamdihakankarakaya@gmail.com>
2020-01-29 14:33:06 +0300

    Fix video sometimes freezes when adjusting speed

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -954,13 +954,23 @@ static int const RCTVideoUnset = -1;
     if (@available(iOS 10.0, *) && !_automaticallyWaitsToMinimizeStalling) {
       [_player playImmediatelyAtRate:_rate];
     } else {
-      [_player play];
+      if([_ignoreSilentSwitch isEqualToString:@"ignore"]) {
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+      } else if([_ignoreSilentSwitch isEqualToString:@"obey"]) {
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient error:nil];
+      }
+
+      if (@available(iOS 10.0, *) && !_automaticallyWaitsToMinimizeStalling) {
+        [_player playImmediatelyAtRate:_rate];
+      } else {
+        [_player play];
+        [_player setRate:_rate];
+      }
       [_player setRate:_rate];
     }
-    [_player setRate:_rate];
+      
+    _paused = paused;
   }
-  
-  _paused = paused;
 }
 
 - (float)getCurrentTime
@@ -1022,6 +1032,15 @@ static int const RCTVideoUnset = -1;
 - (void)setRate:(float)rate
 {
   _rate = rate;
+
+  // Fix video sometimes freezes when adjusting speed
+  dispatch_async(dispatch_get_main_queue(), ^(void){
+    AVPlayerItem *item = _player.currentItem;
+    [_player replaceCurrentItemWithPlayerItem:nil];
+    [_player replaceCurrentItemWithPlayerItem:item];
+    _player.rate = rate;
+  });
+
   [self applyModifiers];
 }
 
