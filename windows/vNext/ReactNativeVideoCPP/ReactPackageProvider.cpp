fff793280096fee2051ec49f7b0b1e79df5fccf5
Di Da <dida@ntdev.microsoft.com>
2020-02-03 09:22:58 -0800

    Fix Cpp/WinRT version and remvoe copyright info

--- windows/vNext/ReactNativeVideoCPP/ReactPackageProvider.cpp
+++ windows/vNext/ReactNativeVideoCPP/ReactPackageProvider.cpp
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 #include "pch.h"
 #include "ReactPackageProvider.h"
 #if __has_include("ReactPackageProvider.g.cpp")
