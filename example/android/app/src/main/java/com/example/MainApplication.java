59a162402a980ee2329830fc3c28cb926db23834
Jens Andersson <jens@fritan.com>
2020-05-15 16:32:29 +0200

    Changed CLI config

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -10,7 +10,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.brentvatne.react.ReactVideoPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          packages.add(new ReactVideoPackage());
+          // packages.add(new MyReactNativePackage());
           return packages;
         }
 
--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  dependencies: {
+    'react-native-video': {
+      root: __dirname,
+    },
+  },
   project: {
     android: {
       sourceDir: './example/android',
+    },
+    ios: {
+      project: './example/ios/example.xcodeproj',
     },
   },
 };
