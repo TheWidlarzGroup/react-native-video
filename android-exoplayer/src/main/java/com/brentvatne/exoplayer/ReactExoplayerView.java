65a30a719e96982c23c68887acefb7923da6da5f
jochem725 <jochemlugtenburg@gmail.com>
2020-07-15 10:34:20 +0200

    Retry on video codec error

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -111,6 +111,7 @@ class ReactExoplayerView extends FrameLayout implements
     private boolean isPaused;
     private boolean isBuffering;
     private float rate = 1f;
+    private int codecRetries = 0;
 
     private int minBufferMs = DefaultLoadControl.DEFAULT_MIN_BUFFER_MS;
     private int maxBufferMs = DefaultLoadControl.DEFAULT_MAX_BUFFER_MS;
@@ -658,6 +659,28 @@ class ReactExoplayerView extends FrameLayout implements
             ex = e.getSourceException();
             errorString = getResources().getString(R.string.unrecognized_media_format);
         }
+
+        // Assumption: If we have a codec error it could be that it is still busy because another component has not yet unmounted.
+        // We retry this max 3 times with some delay and see if the error decreases.
+        int MAX_CODEC_RETRIES = 3;
+        if (e.type == ExoPlaybackException.TYPE_RENDERER) {
+            if (this.codecRetries < MAX_CODEC_RETRIES) {
+                this.codecRetries += 1;
+
+                // Release the current player.
+                releasePlayer();
+
+                // Retry init after some delay.
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        initializePlayer();
+                    }
+                }, 500);
+                return;
+            }
+        }
+
         if (errorString != null) {
             eventEmitter.error(errorString, ex);
         }
