4f9e08b09fd3da6473357ef0c06f46fc597a4afc
Marcel Teixeira <marceltex@gmail.com>
2020-11-05 15:02:21 +0000

    chore: Add additional keys

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactTVExoplayerViewManager.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactTVExoplayerViewManager.java
@@ -29,13 +29,20 @@ public class ReactTVExoplayerViewManager extends ViewGroupManager<ReactTVExoplay
 
     private static final String REACT_CLASS = "RCTVideo";
 
+    // Source properties
     private static final String PROP_SRC = "src";
     private static final String PROP_SRC_URI = "uri";
+    private static final String PROP_SRC_SUBTITLES = "subtitles";
     private static final String PROP_SRC_TYPE = "type";
     private static final String PROP_SRC_DRM = "drm";
-    private static final String PROP_SRC_HEADERS = "requestHeaders";
+    private static final String PROP_SRC_IMA = "ima";
+    private static final String PROP_SRC_METADATA = "metadata";
     private static final String PROP_SRC_CONFIG = "config";
     private static final String PROP_SRC_MUX_DATA = "muxData";
+    private static final String PROP_SRC_MAIN_VERSION = "mainVer";
+    private static final String PROP_SRC_PATCH_VERSION = "patchVer";
+    private static final String PROP_SRC_HEADERS = "requestHeaders";
+
     private static final String PROP_RESIZE_MODE = "resizeMode";
     private static final String PROP_REPEAT = "repeat";
     private static final String PROP_SELECTED_AUDIO_TRACK = "selectedAudioTrack";
@@ -44,7 +51,6 @@ public class ReactTVExoplayerViewManager extends ViewGroupManager<ReactTVExoplay
     private static final String PROP_SELECTED_TEXT_TRACK = "selectedTextTrack";
     private static final String PROP_SELECTED_TEXT_TRACK_TYPE = "type";
     private static final String PROP_SELECTED_TEXT_TRACK_VALUE = "value";
-    private static final String PROP_SUBTITLES = "subtitles";
     private static final String PROP_PAUSED = "paused";
     private static final String PROP_MUTED = "muted";
     private static final String PROP_MEDIA_KEYS = "mediaKeys";
@@ -131,7 +137,7 @@ public class ReactTVExoplayerViewManager extends ViewGroupManager<ReactTVExoplay
         String drm = src.hasKey(PROP_SRC_DRM) ? src.getString(PROP_SRC_DRM) : null;
         Map<String, String> headers = src.hasKey(PROP_SRC_HEADERS) ? toStringMap(src.getMap(PROP_SRC_HEADERS)) : null;
 
-        ReadableArray textTracks = src.hasKey(PROP_SUBTITLES) ? src.getArray(PROP_SUBTITLES) : null;
+        ReadableArray textTracks = src.hasKey(PROP_SRC_SUBTITLES) ? src.getArray(PROP_SRC_SUBTITLES) : null;
         ReadableMap config = src.hasKey(PROP_SRC_CONFIG) ? src.getMap(PROP_SRC_CONFIG) : null;
         ReadableMap muxData = (config != null && config.hasKey(PROP_SRC_MUX_DATA)) ? config.getMap(PROP_SRC_MUX_DATA) : null;
 
