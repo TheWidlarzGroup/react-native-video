9210533a98e604eafd33438fa1ac5af2f8805b1e
Marcel Teixeira <marceltex@gmail.com>
2020-11-09 15:41:48 +0000

    chore: Update types

--- types/translations.ts
+++ types/translations.ts
@@ -5,6 +5,7 @@ export interface IVideoPlayerTranslations {
   player_pause_button: string;
   player_audio_and_subtitles_button: string;
   // new labels
+  live: string;
   favourite: string;
   watchlist: string;
   moreVideos: string;
