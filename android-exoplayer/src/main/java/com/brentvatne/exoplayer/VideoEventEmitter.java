1bb1b2c9e3b2299d2f287948030cdea6b7ea8971
Marcel Teixeira <marceltex@gmail.com>
2020-11-30 15:53:59 +0000

    chore: Remove unnecessary imports

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/VideoEventEmitter.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/VideoEventEmitter.java
@@ -4,7 +4,6 @@ import android.view.View;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
@@ -14,7 +13,6 @@ import com.google.android.exoplayer2.metadata.id3.TextInformationFrame;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.util.Map;
 
 import androidx.annotation.StringDef;
 
