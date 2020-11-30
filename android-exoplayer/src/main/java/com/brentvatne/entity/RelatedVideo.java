74701d0ae1c89547d3804ac3cf34ca75115f043a
Marcel Teixeira <marceltex@gmail.com>
2020-11-30 14:48:40 +0000

    chore: Remove ID property from RelatedVideo

--- android-exoplayer/src/main/java/com/brentvatne/entity/RelatedVideo.java
+++ android-exoplayer/src/main/java/com/brentvatne/entity/RelatedVideo.java
@@ -4,27 +4,20 @@ import java.util.Map;
 
 public class RelatedVideo {
 
-    private final String id;
     private final String title;
     private final String subtitle;
     private final String thumbnailUrl;
     private final Map<String, Object> relatedVideoMap;
 
     public RelatedVideo(
-            String id,
             String title,
             String subtitle,
             String thumbnailUrl,
             Map<String, Object> relatedVideoMap) {
-        this.id = id;
         this.title = title;
         this.subtitle = subtitle;
         this.thumbnailUrl = thumbnailUrl;
         this.relatedVideoMap = relatedVideoMap;
-    }
-
-    public String getId() {
-        return id;
     }
 
     public String getTitle() {
