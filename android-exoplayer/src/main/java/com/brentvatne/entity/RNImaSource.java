9b18750892d90fd9783ba81289cc282a025ecd1e
Marcel Teixeira <marceltex@gmail.com>
2021-02-12 17:23:42 +0000

    chore: Add startDate and endDate to RNImaSource

--- android-exoplayer/src/main/java/com/brentvatne/entity/RNImaSource.java
+++ android-exoplayer/src/main/java/com/brentvatne/entity/RNImaSource.java
@@ -12,19 +12,30 @@ public class RNImaSource {
     private static final String KEY_VIDEO_ID = "videoId";
     private static final String KEY_AUTH_TOKEN = "authToken";
     private static final String KEY_AD_TAG_PARAMETERS = "adTagParameters";
+    private static final String KEY_START_DATE = "startDate";
+    private static final String KEY_END_DATE = "endDate";
 
     private final String assetKey;
     private final String contentSourceId;
     private final String videoId;
     private final String authToken;
-    private final Map<String, Object> adTagParameters;
+
+    private Map<String, Object> adTagParametersMap;
+    private double startDate;
+    private double endDate;
 
     public RNImaSource(@NonNull Map<String, Object> imaSource) {
         this.assetKey = (String) imaSource.get(KEY_ASSET_KEY);
         this.contentSourceId = (String) imaSource.get(KEY_CONTENT_SOURCE_ID);
         this.videoId = (String) imaSource.get(KEY_VIDEO_ID);
         this.authToken = (String) imaSource.get(KEY_AUTH_TOKEN);
-        this.adTagParameters = (Map<String, Object>) imaSource.get(KEY_AD_TAG_PARAMETERS);
+        this.adTagParametersMap = (Map<String, Object>) imaSource.get(KEY_AD_TAG_PARAMETERS);
+        this.startDate = imaSource.get(KEY_START_DATE) != null ?
+                         (double) imaSource.get(KEY_START_DATE) :
+                         Long.MIN_VALUE;
+        this.endDate = imaSource.get(KEY_END_DATE) != null ?
+                       (double) imaSource.get(KEY_END_DATE) :
+                       Long.MAX_VALUE;
     }
 
     @Nullable
@@ -48,7 +59,24 @@ public class RNImaSource {
     }
 
     @Nullable
-    public Map<String, Object> getAdTagParameters() {
-        return adTagParameters;
+    public Map<String, Object> getAdTagParametersMap() {
+        return adTagParametersMap;
+    }
+
+    public double getStartDate() {
+        return startDate;
+    }
+
+    public double getEndDate() {
+        return endDate;
+    }
+
+    public void replaceAdTagParameters(
+            Map<String, Object> adTagParameters,
+            double startDate,
+            double endDate) {
+        this.adTagParametersMap = adTagParameters;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 }
