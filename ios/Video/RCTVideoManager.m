6ee7acba63fc2d462a572744f2c905d25217dd57
Yaroslav Lvov <y.lvov@nullgr.com>
2021-03-01 17:32:51 +0200

    feat: add translations view prop support

--- ios/Video/RCTVideoManager.m
+++ ios/Video/RCTVideoManager.m
@@ -44,6 +44,8 @@ RCT_EXPORT_VIEW_PROPERTY(progressUpdateInterval, float);
 RCT_EXPORT_VIEW_PROPERTY(isFavourite, BOOL);
 RCT_EXPORT_VIEW_PROPERTY(buttons, NSDictionary);
 RCT_EXPORT_VIEW_PROPERTY(theme, NSDictionary);
+RCT_EXPORT_VIEW_PROPERTY(translations, NSDictionary);
+
 
 /* Should support: onLoadStart, onLoad, and onError to stay consistent with Image */
 RCT_EXPORT_VIEW_PROPERTY(onVideoLoadStart, RCTBubblingEventBlock);
