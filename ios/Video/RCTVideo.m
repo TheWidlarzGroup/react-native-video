f5cffc5a01a6d8253eb4ab31e3bbb4b752b2fbd3
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-07 15:53:20 +0200

    Correctly release player instance
    based on https://github.com/react-native-video/react-native-video/pull/1787

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -218,10 +218,11 @@ static int const RCTVideoUnset = -1;
 - (void)dealloc
 {
   [[NSNotificationCenter defaultCenter] removeObserver:self];
-  [self removePlayerLayer];
   [self removePlayerItemObservers];
   [_player removeObserver:self forKeyPath:playbackRate context:nil];
   [_player removeObserver:self forKeyPath:externalPlaybackActive context: nil];
+  [self removePlayerLayer];
+  _player = nil;
 }
 
 #pragma mark - App lifecycle handlers
@@ -360,9 +361,11 @@ static int const RCTVideoUnset = -1;
 - (void)setSrc:(NSDictionary *)source
 {
   _source = source;
-  [self removePlayerLayer];
   [self removePlayerTimeObserver];
   [self removePlayerItemObservers];
+  [self removePlayerLayer];
+  _player = nil;
+
   dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t) 0), dispatch_get_main_queue(), ^{
 
     // perform on next run loop, otherwise other passed react-props may not be set
@@ -1615,9 +1618,6 @@ static int const RCTVideoUnset = -1;
     [_player removeObserver:self forKeyPath:externalPlaybackActive context:nil];
     _isExternalPlaybackActiveObserverRegistered = NO;
   }
-  _player = nil;
-
-  [self removePlayerLayer];
 
   [_playerViewController.contentOverlayView removeObserver:self forKeyPath:@"frame"];
   [_playerViewController removeObserver:self forKeyPath:readyForDisplayKeyPath];
@@ -1631,6 +1631,9 @@ static int const RCTVideoUnset = -1;
 
   _eventDispatcher = nil;
   [[NSNotificationCenter defaultCenter] removeObserver:self];
+
+  [self removePlayerLayer];
+  _player = nil;
 
   [super removeFromSuperview];
 }
