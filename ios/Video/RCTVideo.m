f3d5e4d196668e15082ad60190cf4f3d52604849
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-07 15:47:49 +0200

    Only setup picture in picture controller if `_pictureInPicture` is true
    from @anderslemke (https://github.com/react-native-video/react-native-video/pull/2297)

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -907,7 +907,7 @@ static int const RCTVideoUnset = -1;
 }
 
 - (void)setupPipController {
-  if (!_pipController && _playerLayer && [AVPictureInPictureController isPictureInPictureSupported]) {
+  if (_pictureInPicture && !_pipController && _playerLayer && [AVPictureInPictureController isPictureInPictureSupported]) {
     // Create new controller passing reference to the AVPlayerLayer
     _pipController = [[AVPictureInPictureController alloc] initWithPlayerLayer:_playerLayer];
     _pipController.delegate = self;
