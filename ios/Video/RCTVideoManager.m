0f94a3d956bb501b55c4597c507008014ec2c17c
sdkcy <sidikacay@yahoo.com>
2021-07-26 16:11:37 +0300

    Custom skin support added for ios. onReceiveAdEvent prop added to catch ad events on ios.

--- ios/Video/RCTVideoManager.m
+++ ios/Video/RCTVideoManager.m
@@ -71,6 +71,8 @@ RCT_EXPORT_VIEW_PROPERTY(onPlaybackResume, RCTDirectEventBlock);
 RCT_EXPORT_VIEW_PROPERTY(onPlaybackRateChange, RCTDirectEventBlock);
 RCT_EXPORT_VIEW_PROPERTY(onVideoExternalPlaybackChange, RCTDirectEventBlock);
 RCT_EXPORT_VIEW_PROPERTY(onGetLicense, RCTDirectEventBlock);
+RCT_EXPORT_VIEW_PROPERTY(onReceiveAdEvent, RCTDirectEventBlock);
+
 RCT_REMAP_METHOD(save,
         options:(NSDictionary *)options
         reactTag:(nonnull NSNumber *)reactTag
