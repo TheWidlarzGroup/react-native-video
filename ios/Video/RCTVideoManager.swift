45efd1b26458415d40f7add4d54a0c7bf2b4f18c
Nick <nickfujita@gmail.com>
2021-11-04 16:22:52 +0900

    Initial iOS Swift Conversion

---
+++ ios/Video/RCTVideoManager.swift
@@ -0,0 +1,63 @@
+import AVFoundation
+import React
+
+@objc(RCTVideoManager)
+class RCTVideoManager: RCTViewManager {
+    
+    override func view() -> UIView {
+        return RCTVideo(eventDispatcher: bridge.eventDispatcher())
+    }
+    
+    func methodQueue() -> DispatchQueue {
+        return bridge.uiManager.methodQueue
+    }
+    
+    @objc(save:reactTag:resolver:rejecter:)
+    func save(options: NSDictionary, reactTag: NSNumber, resolve: @escaping RCTPromiseResolveBlock,reject: @escaping RCTPromiseRejectBlock) -> Void {
+        bridge.uiManager.prependUIBlock({_ , viewRegistry in
+            let view = viewRegistry?[reactTag]
+            if !(view is RCTVideo) {
+                RCTLogError("Invalid view returned from registry, expecting RCTVideo, got: %@", String(describing: view))
+            } else if let view = view as? RCTVideo {
+                view.save(options: options, resolve: resolve, reject: reject)
+            }
+        })
+    }
+    
+    @objc(setLicenseResult:reactTag:)
+    func setLicenseResult(license: NSString, reactTag: NSNumber) -> Void {
+        bridge.uiManager.prependUIBlock({_ , viewRegistry in
+            let view = viewRegistry?[reactTag]
+            if !(view is RCTVideo) {
+                RCTLogError("Invalid view returned from registry, expecting RCTVideo, got: %@", String(describing: view))
+            } else if let view = view as? RCTVideo {
+                view.setLicenseResult(license as String)
+            }
+        })
+    }
+    
+    @objc(setLicenseResultError:reactTag:)
+    func setLicenseResultError(error: NSString, reactTag: NSNumber) -> Void {
+        bridge.uiManager.prependUIBlock({_ , viewRegistry in
+            let view = viewRegistry?[reactTag]
+            if !(view is RCTVideo) {
+                RCTLogError("Invalid view returned from registry, expecting RCTVideo, got: %@", String(describing: view))
+            } else if let view = view as? RCTVideo {
+                view.setLicenseResultError(error as String)
+            }
+        })
+    }
+    
+    override func constantsToExport() -> [AnyHashable : Any]? {
+        return [
+            "ScaleNone": AVLayerVideoGravity.resizeAspect,
+            "ScaleToFill": AVLayerVideoGravity.resize,
+            "ScaleAspectFit": AVLayerVideoGravity.resizeAspect,
+            "ScaleAspectFill": AVLayerVideoGravity.resizeAspectFill
+        ]
+    }
+
+    override class func requiresMainQueueSetup() -> Bool {
+        return true
+    }
+}
--- react-native-video.podspec
+++ react-native-video.podspec
@@ -12,12 +12,12 @@ Pod::Spec.new do |s|
   s.homepage       = 'https://github.com/react-native-community/react-native-video'
   s.source       = { :git => "https://github.com/react-native-community/react-native-video.git", :tag => "#{s.version}" }
 
-  s.ios.deployment_target = "8.0"
+  s.ios.deployment_target = "9.0"
   s.tvos.deployment_target = "9.0"
 
   s.subspec "Video" do |ss|
-    ss.source_files  = "ios/Video/*.{h,m}"
-    s.static_framework = true
+    ss.source_files  = "ios/Video/**/*.{h,m,swift}"
+    ss.dependency "PromisesSwift"
   end
 
   s.subspec "VideoCaching" do |ss|
@@ -25,13 +25,14 @@ Pod::Spec.new do |s|
     ss.dependency "SPTPersistentCache", "~> 1.1.0"
     ss.dependency "DVAssetLoaderDelegate", "~> 0.3.1"
 
-    ss.source_files = "ios/VideoCaching/**/*.{h,m}"
-    s.static_framework = true
+    ss.source_files = "ios/VideoCaching/**/*.{h,m,swift}"
   end
 
   s.dependency "React-Core"
 
   s.default_subspec = "Video"
+
+  s.static_framework = true
 
   s.xcconfig = {
     'OTHER_LDFLAGS': '-ObjC',
