933af3b6e2a03039418839db072c2a5639bcf960
Roman Savka <roman.savka@miquido.com>
2021-12-03 10:17:43 +0100

    Set render PREFERED when device supports audio/eac3

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/DolbyDefaultExtensionMode.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/DolbyDefaultExtensionMode.java
@@ -3,7 +3,6 @@ package com.brentvatne.exoplayer;
 import static android.os.Build.VERSION.SDK_INT;
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
 import static com.google.android.exoplayer2.DefaultRenderersFactory.EXTENSION_RENDERER_MODE_OFF;
-import static com.google.android.exoplayer2.DefaultRenderersFactory.EXTENSION_RENDERER_MODE_ON;
 import static com.google.android.exoplayer2.DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER;
 
 import android.media.MediaCodecInfo;
@@ -19,7 +18,7 @@ public class DolbyDefaultExtensionMode {
 
     @ExtensionRendererMode
     public static int getDefaultMode() {
-        //check the AC4 and DDP decoder on the device
+        // check the AC4 decoder on the device
         MediaCodecList list = null;
         if (SDK_INT >= LOLLIPOP) {
             list = new MediaCodecList(MediaCodecList.ALL_CODECS);
@@ -28,7 +27,6 @@ public class DolbyDefaultExtensionMode {
         if (SDK_INT >= LOLLIPOP) {
             codecInfos = list.getCodecInfos();
         }
-        boolean dlbDDPDevice = false;
         boolean dlbAC4Device = false;
 
         for (MediaCodecInfo info : codecInfos) {
@@ -39,23 +37,15 @@ public class DolbyDefaultExtensionMode {
             for (String type : info.getSupportedTypes()) {
                 if (type.equals("audio/ac4")) {
                     dlbAC4Device = true;
-                } else if (type.equals("audio/eac3")) {
-                    dlbDDPDevice = true;
+                    break;
                 }
             }
         }
 
         if (dlbAC4Device) {
             Log.i("DAA", "This is a Dolby licensed device with Dolby AC-4 and DDP decoders.");
-            Log.i("DAA",
-                    "Player should bypass Dolby Audio library and use the existing decoders on the device.");
+            Log.i("DAA", "Player should bypass Dolby Audio library and use the existing decoders on the device.");
             return EXTENSION_RENDERER_MODE_OFF;
-        } else if (dlbDDPDevice) {
-            Log.i("DAA", "This is a Dolby licensed device with DDP decoder only.");
-            Log.i("DAA", "For Dolby AC-4 content, player should use Dolby Audio library for decoding.");
-            Log.i("DAA",
-                    "For DDP content, player should bypass Dolby Audio library and use the decoder on the device.");
-            return EXTENSION_RENDERER_MODE_ON;
         } else {
             Log.i("DAA", "This is not a Dolby licensed device.");
             Log.i("DAA", "Player should use Dolby Audio library for decoding Dolby content.");
