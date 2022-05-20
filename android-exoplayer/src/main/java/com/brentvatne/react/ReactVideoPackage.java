36efae5e5a16bf44f3b0cad016269bb3bd6fcebc
Alex Sevrukov <sevrukovaleksej953@gmail.com>
2022-05-20 22:04:51 +0300

    Revert "add missing import"
    This reverts commit 7a9e2249c9fdc24b3effb8688827b090c3c5394c.

--- android-exoplayer/src/main/java/com/brentvatne/react/ReactVideoPackage.java
+++ android-exoplayer/src/main/java/com/brentvatne/react/ReactVideoPackage.java
@@ -10,7 +10,6 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
