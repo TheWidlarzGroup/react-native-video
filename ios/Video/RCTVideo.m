caa2dcad9ccf1f13d13d0b01c88a006b356b2c8f
Adam Cobb <cobb.2326@gmail.com>
2023-02-23 14:58:23 -0800

    Fix iOS PiP opening by default
    Fixes https://github.com/react-native-video/react-native-video/issues/2177

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -907,10 +907,12 @@ static int const RCTVideoUnset = -1;
 }
 
 - (void)setupPipController {
-  if (!_pipController && _playerLayer && [AVPictureInPictureController isPictureInPictureSupported]) {
+  if (!_pipController && _playerLayer && _pictureInPicture && [AVPictureInPictureController isPictureInPictureSupported]) {
     // Create new controller passing reference to the AVPlayerLayer
     _pipController = [[AVPictureInPictureController alloc] initWithPlayerLayer:_playerLayer];
     _pipController.delegate = self;
+  } else {
+    _pipController = NULL;
   }
 }
 #endif
