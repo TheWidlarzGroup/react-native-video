4ddecbf42b88b6d7e2ec993f57a310591c801161
yungblud <killi8n@gmail.com>
2023-04-13 16:20:08 +0900

    feat: :zap: modified setup

--- examples/LaftelVideo/babel.config.js
+++ examples/LaftelVideo/babel.config.js
@@ -9,7 +9,7 @@ module.exports = {
       {
         extensions: ['.tsx', '.ts', '.js', '.json'],
         alias: {
-          [pak.name]: path.join(__dirname, '..', pak.source),
+          [pak.name]: path.join(__dirname, '../../', pak.source),
         },
       },
     ],
