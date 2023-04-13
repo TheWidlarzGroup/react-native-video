e3697d7ea5e9ec06e72d1994df98e2cdd89b562e
yungblud <killi8n@gmail.com>
2023-04-13 14:44:38 +0900

    feat: :zap: set path

--- examples/LaftelVideo/metro.config.js
+++ examples/LaftelVideo/metro.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const escape = require('escape-string-regexp');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
-const pak = require('../package.json');
+const pak = require('../../package.json');
 
 const root = path.resolve(__dirname, '../..');
 
