902ba2f2fc1e09279f926e9131a5d7cb62bf5fd2
yungblud <killi8n@gmail.com>
2023-04-13 18:24:15 +0900

    feat: :zap: modified RCTVideo.h

--- ios/Fabric/RCTVideo.h
+++ ios/Fabric/RCTVideo.h
@@ -61,6 +61,7 @@ typedef NS_ENUM(NSInteger, RCTVideoError) {
 
 - (AVPlayerViewController*)createPlayerViewController:(AVPlayer*)player withPlayerItem:(AVPlayerItem*)playerItem;
 
+- (void)setSrc:(NSDictionary *)source;
 - (void)save:(NSDictionary *)options resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
 - (void)setLicenseResult:(NSString * )license;
 - (BOOL)setLicenseResultError:(NSString * )error;
