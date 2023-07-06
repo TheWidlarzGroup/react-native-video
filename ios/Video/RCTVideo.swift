d526479fe0983cbd1aa1a4a6ab1dd1ba82e85bb7
Cedric Guinoiseau <c.guinoiseau@beapp.fr>
2023-07-06 09:37:02 +0200

    fix: issue 3040, prevent crash

--- ios/Video/RCTVideo.swift
+++ ios/Video/RCTVideo.swift
@@ -665,6 +665,11 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
                 self.onVideoFullscreenPlayerWillPresent?(["target": reactTag as Any])
 
                 if let playerViewController = _playerViewController {
+                    if(_controls) {
+                        // prevents crash https://github.com/react-native-video/react-native-video/issues/3040
+                        self._playerViewController?.removeFromParent()
+                    }
+
                     viewController.present(playerViewController, animated:true, completion:{
                         self._playerViewController?.showsPlaybackControls = self._controls
                         self._fullscreenPlayerPresented = fullscreen
