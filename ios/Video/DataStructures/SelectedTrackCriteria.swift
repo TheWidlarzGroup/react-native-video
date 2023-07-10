168eb95d5b7e633fc1b1d58fee305a943e8027cc
yungblud <killi8n@gmail.com>
2023-07-10 19:59:09 +0900

    feat: selected type

--- ios/Video/DataStructures/SelectedTrackCriteria.swift
+++ ios/Video/DataStructures/SelectedTrackCriteria.swift
@@ -12,7 +12,16 @@ struct SelectedTrackCriteria {
             return
         }
         self.json = json
+        #if RCT_NEW_ARCH_ENABLED
         self.type = json["type"] as? String ?? ""
+        #else
+        if json["selectedAudioType"] as? String != nil {
+            self.type = json["selectedAudioType"] as? String ?? ""
+        }
+        if json["selectedTextType"] as? String != nil {
+            self.type = json["selectedTextType"] as? String ?? ""
+        }
+        #endif
         self.value = json["value"]
     }
 }
