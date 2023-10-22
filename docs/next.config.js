18fe2f95622c50b48aa1761ace843778448de619
Krzysztof Moch <krzysmoch.programs@gmail.com>
2023-10-22 16:42:06 +0200

    fix(docs): update next config

--- docs/next.config.js
+++ docs/next.config.js
@@ -5,8 +5,18 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.jsx',
 });
 
-let assetPrefix = '';
+let assetPrefix = './';
 let basePath = '';
+
+// If we're in a GitHub Action, we need to set the assetPrefix and basePath
+// to add repo_name to the path.
+// eg. https://<organization>.github.io/<repo_name>
+if (process.env.GITHUB_ACTIONS) {
+  const repo_name = 'react-native-video';
+
+  assetPrefix = `/${repo_name}/`;
+  basePath = `/${repo_name}`;
+}
 
 module.exports = withNextra({
   output: 'export',
