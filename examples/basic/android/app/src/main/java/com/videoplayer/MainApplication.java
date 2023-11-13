fc0076b7bc1bfe71d44a14d397de7d51b24a6b95
Krzysztof Moch <krzysmoch.programs@gmail.com>
2023-11-13 12:01:44 +0100

    chore(examples/basic): change manual linking for react-native-video

--- examples/basic/android/app/src/main/java/com/videoplayer/MainApplication.java
+++ examples/basic/android/app/src/main/java/com/videoplayer/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import com.videoplayer.newarchitecture.MainApplicationReactNativeHost;
+import com.brentvatne.react.ReactVideoPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+          packages.add(new ReactVideoPackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
