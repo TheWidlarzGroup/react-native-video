136d71abad0c48b2db4f6cb436bfcb94b63b9bea
olivier <olivier.bouillet@ifeelsmart.com>
2023-11-19 22:58:31 +0100

    chore: fix app startup due to autolinking

--- examples/basic/android/app/src/main/java/com/videoplayer/MainApplication.java
+++ examples/basic/android/app/src/main/java/com/videoplayer/MainApplication.java
@@ -29,7 +29,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          packages.add(new ReactVideoPackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
