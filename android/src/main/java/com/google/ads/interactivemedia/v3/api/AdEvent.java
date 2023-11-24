3a04d476d472101e3aec8ccbdf881298741ee972
Axel Vencatareddy <axel@tilted.fr>
2023-11-24 11:13:00 +0100

    fix: add getAdData to stub file

--- android/src/main/java/com/google/ads/interactivemedia/v3/api/AdEvent.java
+++ android/src/main/java/com/google/ads/interactivemedia/v3/api/AdEvent.java
@@ -4,6 +4,7 @@ import androidx.annotation.InspectableProperty;
 
 public abstract class AdEvent {
     public abstract InspectableProperty getType();
+    public abstract InspectableProperty getAdData();
 
     public interface AdEventListener {
         public void onAdEvent(AdEvent adEvent);
