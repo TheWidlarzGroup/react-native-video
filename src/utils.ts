66d4df3aea0b30746d26a9b29eff143167114f32
Krzysztof Moch <krzysmoch.programs@gmail.com>
2024-03-09 15:54:54 +0100

    fix types

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,6 @@
 import type {Component, RefObject, ComponentClass} from 'react';
-import {Image, findNodeHandle} from 'react-native';
+import {Image, findNodeHandle, type ImageSourcePropType} from 'react-native';
 import type {ReactVideoSource, ReactVideoSourceProperties} from './types/video';
-
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function generateHeaderForNative(obj?: Record<string, any>) {
@@ -12,8 +11,9 @@ export function generateHeaderForNative(obj?: Record<string, any>) {
 }
 
 type Source = ImageSourcePropType | ReactVideoSource;
+
 export function resolveAssetSourceForVideo(
-  source: ReactVideoSource,
+  source: Source,
 ): ReactVideoSourceProperties {
   // This is deprecated, but we need to support it for backward compatibility
   if (typeof source === 'number') {
@@ -22,7 +22,7 @@ export function resolveAssetSourceForVideo(
     };
   }
 
-  if (typeof source.uri === 'number') {
+  if ('uri' in source && typeof source.uri === 'number') {
     return {
       ...source,
       uri: Image.resolveAssetSource(source.uri).uri,
