6184c10acc90defd63cd55af51458864dfe112d5
coofzilla <75731066+coofzilla@users.noreply.github.com>
2024-03-11 21:50:19 +0900

    feat(android): add subtitle event (#3566)
    * feat: add onCues method to get access to subtitle content

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -41,6 +41,7 @@ import androidx.media3.common.Timeline;
 import androidx.media3.common.TrackGroup;
 import androidx.media3.common.TrackSelectionOverride;
 import androidx.media3.common.Tracks;
+import androidx.media3.common.text.CueGroup;
 import androidx.media3.common.util.Util;
 import androidx.media3.datasource.DataSource;
 import androidx.media3.datasource.DataSpec;
@@ -1519,6 +1520,13 @@ public class ReactExoplayerView extends FrameLayout implements
             }
         }
         eventEmitter.timedMetadata(metadataArray);
+    }
+
+    public void onCues(CueGroup cueGroup) {
+        if (!cueGroup.cues.isEmpty() && cueGroup.cues.get(0).text != null) {
+            String subtitleText = cueGroup.cues.get(0).text.toString();
+            eventEmitter.textTrackDataChanged(subtitleText);
+        }
     }
 
     // ReactExoplayerViewManager public api
