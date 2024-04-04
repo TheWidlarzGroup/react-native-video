10b100de44830f8c6d502ddbaae6d6c72c04f625
Krzysztof Moch <krzysmoch.programs@gmail.com>
2024-04-04 13:23:44 +0200

    feat!(ios): remove native dependency `promises` (#3631)

--- ios/VideoCaching/RCTVideoCachingHandler.swift
+++ ios/VideoCaching/RCTVideoCachingHandler.swift
@@ -1,11 +1,10 @@
 import AVFoundation
 import DVAssetLoaderDelegate
 import Foundation
-import Promises
 
 class RCTVideoCachingHandler: NSObject, DVAssetLoaderDelegatesDelegate {
     private var _videoCache: RCTVideoCache! = RCTVideoCache.sharedInstance()
-    var playerItemPrepareText: ((AVAsset?, NSDictionary?, String) -> Promise<AVPlayerItem>)?
+    var playerItemPrepareText: ((AVAsset?, NSDictionary?, String) async -> AVPlayerItem)?
 
     override init() {
         super.init()
@@ -26,69 +25,65 @@ class RCTVideoCachingHandler: NSObject, DVAssetLoaderDelegatesDelegate {
         return false
     }
 
-    func playerItemForSourceUsingCache(uri: String!, assetOptions options: NSDictionary!) -> Promise<AVPlayerItem> {
+    func playerItemForSourceUsingCache(uri: String!, assetOptions options: NSDictionary!) async throws -> AVPlayerItem {
         let url = URL(string: uri)
-        return getItemForUri(uri)
-            .then { [weak self] (videoCacheStatus: RCTVideoCacheStatus, cachedAsset: AVAsset?) -> Promise<AVPlayerItem> in
-                guard let self, let playerItemPrepareText = self.playerItemPrepareText else { throw NSError(domain: "", code: 0, userInfo: nil) }
-                switch videoCacheStatus {
-                case .missingFileExtension:
-                    DebugLog("""
-                      Could not generate cache key for uri '\(uri)'.
-                      It is currently not supported to cache urls that do not include a file extension.
-                      The video file will not be cached.
-                      Checkout https://github.com/react-native-community/react-native-video/blob/master/docs/caching.md
-                    """)
-                    let asset: AVURLAsset! = AVURLAsset(url: url!, options: options as! [String: Any])
-                    return playerItemPrepareText(asset, options, "")
+        let (videoCacheStatus, cachedAsset) = await getItemForUri(uri)
 
-                case .unsupportedFileExtension:
-                    DebugLog("""
-                      Could not generate cache key for uri '\(uri)'.
-                      The file extension of that uri is currently not supported.
-                      The video file will not be cached.
-                      Checkout https://github.com/react-native-community/react-native-video/blob/master/docs/caching.md
-                    """)
-                    let asset: AVURLAsset! = AVURLAsset(url: url!, options: options as! [String: Any])
-                    return playerItemPrepareText(asset, options, "")
+        guard let playerItemPrepareText else {
+            throw NSError(domain: "", code: 0, userInfo: nil)
+        }
 
-                default:
-                    if let cachedAsset {
-                        DebugLog("Playing back uri '\(uri)' from cache")
-                        // See note in playerItemForSource about not being able to support text tracks & caching
-                        return Promise {
-                            AVPlayerItem(asset: cachedAsset)
-                        }
-                    }
-                }
+        switch videoCacheStatus {
+        case .missingFileExtension:
+            DebugLog("""
+              Could not generate cache key for uri '\(uri ?? "NO_URI")'.
+              It is currently not supported to cache urls that do not include a file extension.
+              The video file will not be cached.
+              Checkout https://github.com/react-native-community/react-native-video/blob/master/docs/caching.md
+            """)
+            let asset: AVURLAsset! = AVURLAsset(url: url!, options: options as! [String: Any])
+            return await playerItemPrepareText(asset, options, "")
 
-                let asset: DVURLAsset! = DVURLAsset(url: url, options: options as! [String: Any], networkTimeout: 10000)
-                asset.loaderDelegate = self
+        case .unsupportedFileExtension:
+            DebugLog("""
+              Could not generate cache key for uri '\(uri ?? "NO_URI")'.
+              The file extension of that uri is currently not supported.
+              The video file will not be cached.
+              Checkout https://github.com/react-native-community/react-native-video/blob/master/docs/caching.md
+            """)
+            let asset: AVURLAsset! = AVURLAsset(url: url!, options: options as! [String: Any])
+            return await playerItemPrepareText(asset, options, "")
 
-                /* More granular code to have control over the DVURLAsset
-                 let resourceLoaderDelegate = DVAssetLoaderDelegate(url: url)
-                 resourceLoaderDelegate.delegate = self
-                 let components = NSURLComponents(url: url, resolvingAgainstBaseURL: false)
-                 components?.scheme = DVAssetLoaderDelegate.scheme()
-                 var asset: AVURLAsset? = nil
-                 if let url = components?.url {
-                 asset = AVURLAsset(url: url, options: options)
-                 }
-                 asset?.resourceLoader.setDelegate(resourceLoaderDelegate, queue: DispatchQueue.main)
-                 */
-
-                return Promise {
-                    AVPlayerItem(asset: asset)
-                }
-            }.then { playerItem -> AVPlayerItem in
-                return playerItem
+        default:
+            if let cachedAsset {
+                DebugLog("Playing back uri '\(uri ?? "NO_URI")' from cache")
+                // See note in playerItemForSource about not being able to support text tracks & caching
+                return AVPlayerItem(asset: cachedAsset)
             }
+        }
+
+        let asset: DVURLAsset! = DVURLAsset(url: url, options: options as! [String: Any], networkTimeout: 10000)
+        asset.loaderDelegate = self
+
+        /* More granular code to have control over the DVURLAsset
+         let resourceLoaderDelegate = DVAssetLoaderDelegate(url: url)
+         resourceLoaderDelegate.delegate = self
+         let components = NSURLComponents(url: url, resolvingAgainstBaseURL: false)
+         components?.scheme = DVAssetLoaderDelegate.scheme()
+         var asset: AVURLAsset? = nil
+         if let url = components?.url {
+         asset = AVURLAsset(url: url, options: options)
+         }
+         asset?.resourceLoader.setDelegate(resourceLoaderDelegate, queue: DispatchQueue.main)
+         */
+
+        return AVPlayerItem(asset: asset)
     }
 
-    func getItemForUri(_ uri: String) -> Promise<(videoCacheStatus: RCTVideoCacheStatus, cachedAsset: AVAsset?)> {
-        return Promise<(videoCacheStatus: RCTVideoCacheStatus, cachedAsset: AVAsset?)> { fulfill, _ in
+    func getItemForUri(_ uri: String) async -> (videoCacheStatus: RCTVideoCacheStatus, cachedAsset: AVAsset?) {
+        await withCheckedContinuation { continuation in
             self._videoCache.getItemForUri(uri, withCallback: { (videoCacheStatus: RCTVideoCacheStatus, cachedAsset: AVAsset?) in
-                fulfill((videoCacheStatus, cachedAsset))
+                continuation.resume(returning: (videoCacheStatus, cachedAsset))
             })
         }
     }
