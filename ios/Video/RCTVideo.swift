b8509c25780c56629aaeaf99c4f98fa28edb0f8c
Olivier Bouillet <freeboub@gmail.com>
2024-04-25 23:11:21 +0200

    fix(iOS): ensure orientation is correctly reported

--- ios/Video/RCTVideo.swift
+++ ios/Video/RCTVideo.swift
@@ -1285,8 +1285,8 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
             duration = 0.0
         }
 
-        var width: Float?
-        var height: Float?
+        var width: Float = 0
+        var height: Float = 0
         var orientation = "undefined"
 
         Task {
@@ -1294,20 +1294,11 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
             if let videoTrack = tracks?.first {
                 width = Float(videoTrack.naturalSize.width)
                 height = Float(videoTrack.naturalSize.height)
-                let preferredTransform = videoTrack.preferredTransform
-
-                if (videoTrack.naturalSize.width == preferredTransform.tx
-                    && videoTrack.naturalSize.height == preferredTransform.ty)
-                    || (preferredTransform.tx == 0 && preferredTransform.ty == 0) {
-                    orientation = "landscape"
-                } else {
-                    orientation = "portrait"
-                }
             } else if _playerItem.presentationSize.height != 0.0 {
                 width = Float(_playerItem.presentationSize.width)
                 height = Float(_playerItem.presentationSize.height)
-                orientation = _playerItem.presentationSize.width > _playerItem.presentationSize.height ? "landscape" : "portrait"
             }
+            orientation = width > height ? "landscape" : width == height ? "square" : "portrait"
 
             if self._pendingSeek {
                 self.setSeek([
