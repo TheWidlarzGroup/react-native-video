5edde970e26333d3d9269cb91c494861904a4f03
Dibyendu Das <dibyendu.das.in@gmail.com>
2024-06-06 17:43:00 -0400

    Patch commit to fix the error:   Implicit use of 'self' in closure; use 'self.' to make capture semantics explicit

--- ios/Video/NowPlayingInfoCenterManager.swift
+++ ios/Video/NowPlayingInfoCenterManager.swift
@@ -150,7 +150,7 @@ class NowPlayingInfoCenterManager {
             guard let self, let player = self.currentPlayer else {
                 return .commandFailed
             }
-            let newTime = player.currentTime() - CMTime(seconds: SEEK_INTERVAL_SECONDS, preferredTimescale: .max)
+            let newTime = player.currentTime() - CMTime(seconds: self.SEEK_INTERVAL_SECONDS, preferredTimescale: .max)
             player.seek(to: newTime)
             return .success
         }
@@ -160,7 +160,7 @@ class NowPlayingInfoCenterManager {
                 return .commandFailed
             }
 
-            let newTime = player.currentTime() + CMTime(seconds: SEEK_INTERVAL_SECONDS, preferredTimescale: .max)
+            let newTime = player.currentTime() + CMTime(seconds: self.SEEK_INTERVAL_SECONDS, preferredTimescale: .max)
             player.seek(to: newTime)
             return .success
         }
@@ -266,15 +266,15 @@ class NowPlayingInfoCenterManager {
 
             // case where there is new player that is not paused
             // In this case event is triggered by non currentPlayer
-            if rate != 0 && currentPlayer != player {
-                setCurrentPlayer(player: player)
+            if rate != 0 && self.currentPlayer != player {
+                self.setCurrentPlayer(player: player)
                 return
             }
 
             // case where currentPlayer was paused
             // In this case event is triggeret by currentPlayer
-            if rate == 0 && currentPlayer == player {
-                findNewCurrentPlayer()
+            if rate == 0 && self.currentPlayer == player {
+                self.findNewCurrentPlayer()
             }
         }
     }
