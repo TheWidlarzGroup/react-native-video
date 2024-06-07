77cb0e1b1c13232c16bd488261db4b654ff667b8
Olivier Bouillet <freeboub@gmail.com>
2024-06-07 09:32:32 +0200

    feat(android): add a way to disable chuncklessPreparation on HLS

--- android/src/main/java/com/brentvatne/common/api/Source.kt
+++ android/src/main/java/com/brentvatne/common/api/Source.kt
@@ -9,6 +9,7 @@ import android.text.TextUtils
 import com.brentvatne.common.toolbox.DebugLog
 import com.brentvatne.common.toolbox.DebugLog.e
 import com.brentvatne.common.toolbox.ReactBridgeUtils.safeGetArray
+import com.brentvatne.common.toolbox.ReactBridgeUtils.safeGetBool
 import com.brentvatne.common.toolbox.ReactBridgeUtils.safeGetInt
 import com.brentvatne.common.toolbox.ReactBridgeUtils.safeGetMap
 import com.brentvatne.common.toolbox.ReactBridgeUtils.safeGetString
@@ -43,6 +44,11 @@ class Source {
 
     /** http header list */
     val headers: MutableMap<String, String> = HashMap()
+
+    /** enable chunckless preparation for HLS
+     * see:
+     */
+    var textTracksAllowChuncklessPreparation: Boolean = false
 
     /** return true if this and src are equals  */
     override fun equals(other: Any?): Boolean {
@@ -114,6 +120,7 @@ class Source {
         private const val PROP_SRC_TYPE = "type"
         private const val PROP_SRC_METADATA = "metadata"
         private const val PROP_SRC_HEADERS = "requestHeaders"
+        private const val PROP_SRC_TEXT_TRACKS_ALLOW_CHUNCKLESS_PREPARATION = "textTracksAllowChunklessPreparation"
 
         @SuppressLint("DiscouragedApi")
         private fun getUriFromAssetId(context: Context, uriString: String): Uri? {
@@ -170,6 +177,7 @@ class Source {
                 source.cropStartMs = safeGetInt(src, PROP_SRC_CROP_START, -1)
                 source.cropEndMs = safeGetInt(src, PROP_SRC_CROP_END, -1)
                 source.extension = safeGetString(src, PROP_SRC_TYPE, null)
+                source.textTracksAllowChuncklessPreparation = safeGetBool(src, PROP_SRC_TEXT_TRACKS_ALLOW_CHUNCKLESS_PREPARATION, true)
 
                 val propSrcHeadersArray = safeGetArray(src, PROP_SRC_HEADERS)
                 if (propSrcHeadersArray != null) {
