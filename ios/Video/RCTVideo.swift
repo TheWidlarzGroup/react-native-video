8fd8664fde7924ac6fd2df647862be6a798f6a8b
Olivier Bouillet <freeboub@gmail.com>
2024-06-14 14:18:57 +0200

    fix: ensure ads manager doesn't crash after leaving the app

--- ios/Video/RCTVideo.swift
+++ ios/Video/RCTVideo.swift
@@ -247,6 +247,11 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
     }
 
     deinit {
+        #if USE_GOOGLE_IMA
+            _imaAdsManager.releaseAds()
+            _imaAdsManager = nil
+        #endif
+
         NotificationCenter.default.removeObserver(self)
         self.removePlayerLayer()
         _playerObserver.clearPlayer()
@@ -1258,11 +1263,6 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
         _player = nil
         _resouceLoaderDelegate = nil
         _playerObserver.clearPlayer()
-
-        #if USE_GOOGLE_IMA
-            _imaAdsManager.releaseAds()
-            _imaAdsManager = nil
-        #endif
 
         self.removePlayerLayer()
 
