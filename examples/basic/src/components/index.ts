e484ee09fe86815d04dcfe2268bd2fadc1ff37c3
mostafahasani <seyedmostafahassani@gmail.com>
2024-06-23 23:39:12 +0330

    refactor: basic example from class component to functional component

---
+++ examples/basic/src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './Indicator';
+export * from './Seeker';
+export * from './AudioTracksSelector';
+export * from './VideoTracksSelector';
+export * from './TextTracksSelector';
+export * from './Overlay';
+export * from './TopControl';
