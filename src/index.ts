facaba09d7438a48dc0106318133944a161a0876
Krzysztof Moch <krzysmoch.programs@gmail.com>
2024-06-23 16:03:50 +0200

    add export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Video from './Video';
 export {VideoDecoderProperties} from './specs/VideoNativeComponent';
+export {default as withRNVideo} from './expo-plugins/withRNVideo';
 export * from './types';
 export type {VideoRef} from './Video';
 export default Video;
