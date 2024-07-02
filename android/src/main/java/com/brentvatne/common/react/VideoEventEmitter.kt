09ce87db3e227a6e9f7b39856f5f8b37344e92f5
YangJH <jhaugusty@gmail.com>
2024-07-02 16:28:10 +0900

    refactor(android): rename of event constants for Fabric
    - https://github.com/facebook/react-native/blob/v0.74.3/packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/UIManagerModuleConstantsHelper.java#L136-L138

--- android/src/main/java/com/brentvatne/common/react/VideoEventEmitter.kt
+++ android/src/main/java/com/brentvatne/common/react/VideoEventEmitter.kt
@@ -48,7 +48,7 @@ enum class EventTypes(val eventName: String) {
         fun toMap() =
             mutableMapOf<String, Any>().apply {
                 EventTypes.entries.forEach { eventType ->
-                    put(eventType.eventName, mapOf("registrationName" to eventType.eventName))
+                    put("top${eventType.eventName.removePrefix("on")}", mapOf("registrationName" to eventType.eventName))
                 }
             }
     }
@@ -279,7 +279,7 @@ class VideoEventEmitter {
     private class EventBuilder(private val surfaceId: Int, private val viewId: Int, private val dispatcher: EventDispatcher) {
         fun dispatch(event: EventTypes, paramsSetter: (WritableMap.() -> Unit)? = null) =
             dispatcher.dispatchEvent(object : Event<Event<*>>(surfaceId, viewId) {
-                override fun getEventName() = event.eventName
+                override fun getEventName() = "top${event.eventName.removePrefix("on")}"
                 override fun getEventData() = Arguments.createMap().apply(paramsSetter ?: {})
             })
     }
