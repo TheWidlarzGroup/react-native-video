b3d9846b3bac033dc7e45d76b73526a5699918e9
mostafahasani <seyedmostafahassani@gmail.com>
2024-07-11 23:57:48 +0330

    refactor(android): migrate ReactExoplayerConfig to Kotlin

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerConfig.kt
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerConfig.kt
@@ -1,16 +1,10 @@
-package com.brentvatne.exoplayer;
+package com.brentvatne.exoplayer
 
-import androidx.media3.exoplayer.upstream.DefaultBandwidthMeter;
-import androidx.media3.exoplayer.upstream.LoadErrorHandlingPolicy;
+import androidx.media3.exoplayer.upstream.DefaultBandwidthMeter
+import androidx.media3.exoplayer.upstream.LoadErrorHandlingPolicy
 
-/**
- * Extension points to configure the Exoplayer instance
- */
-public interface ReactExoplayerConfig {
-    LoadErrorHandlingPolicy buildLoadErrorHandlingPolicy(int minLoadRetryCount);
-
-    void setDisableDisconnectError(boolean disableDisconnectError);
-    boolean getDisableDisconnectError();
-
-    DefaultBandwidthMeter getBandwidthMeter();
+interface ReactExoplayerConfig {
+    fun buildLoadErrorHandlingPolicy(minLoadRetryCount: Int): LoadErrorHandlingPolicy
+    var disableDisconnectError: Boolean
+    val bandwidthMeter: DefaultBandwidthMeter
 }
