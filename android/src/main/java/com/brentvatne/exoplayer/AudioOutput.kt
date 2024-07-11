96e410f9043a6b5e2c69b7e51e4e1e6063c7895b
mostafahasani <seyedmostafahassani@gmail.com>
2024-07-11 23:20:51 +0330

    fix: lint error

--- android/src/main/java/com/brentvatne/exoplayer/AudioOutput.kt
+++ android/src/main/java/com/brentvatne/exoplayer/AudioOutput.kt
@@ -6,14 +6,14 @@ import androidx.media3.common.C
 @SuppressLint("InlinedApi")
 enum class AudioOutput(private val mName: String, @C.StreamType val streamType: Int) {
 
-    SPEAKER("speaker", C.STREAM_TYPE_MUSIC ),
+    SPEAKER("speaker", C.STREAM_TYPE_MUSIC),
     EARPIECE("earpiece", C.STREAM_TYPE_VOICE_CALL);
 
     companion object {
         @JvmStatic
-        fun get(name: String) : AudioOutput {
-            for (d in entries){
-                if (d.mName.equals(name, ignoreCase = true)){
+        fun get(name: String): AudioOutput {
+            for (d in entries) {
+                if (d.mName.equals(name, ignoreCase = true)) {
                     return d
                 }
             }
@@ -21,8 +21,5 @@ enum class AudioOutput(private val mName: String, @C.StreamType val streamType: 
         }
     }
 
-    override fun toString(): String {
-        return "${javaClass.simpleName}($mName, $streamType)"
-    }
-
+    override fun toString(): String = "${javaClass.simpleName}($mName, $streamType)"
 }
