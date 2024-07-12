b5d38cd7bf9d3d188919a612afaeb670d2335b59
mostafahasani <seyedmostafahassani@gmail.com>
2024-07-12 16:54:50 +0330

    chore: initialize appContext with context.applicationContext

--- android/src/main/java/com/brentvatne/receiver/AudioBecomingNoisyReceiver.kt
+++ android/src/main/java/com/brentvatne/receiver/AudioBecomingNoisyReceiver.kt
@@ -7,12 +7,9 @@ import android.content.IntentFilter
 import android.media.AudioManager
 import androidx.core.content.ContextCompat
 
-class AudioBecomingNoisyReceiver(private val context: Context) : BroadcastReceiver() {
+class AudioBecomingNoisyReceiver(context: Context) : BroadcastReceiver() {
     private var listener: BecomingNoisyListener = BecomingNoisyListener.NO_OP
-
-    init {
-        context.applicationContext
-    }
+    private val appContext: Context = context.applicationContext
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent != null) {
             if (AudioManager.ACTION_AUDIO_BECOMING_NOISY == intent.action) {
@@ -24,13 +21,13 @@ class AudioBecomingNoisyReceiver(private val context: Context) : BroadcastReceiv
     fun setListener(listener: BecomingNoisyListener) {
         this.listener = listener
         val intentFilter = IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY)
-        ContextCompat.registerReceiver(context, this, intentFilter, ContextCompat.RECEIVER_NOT_EXPORTED)
+        ContextCompat.registerReceiver(appContext, this, intentFilter, ContextCompat.RECEIVER_NOT_EXPORTED)
     }
 
     fun removeListener() {
         this.listener = BecomingNoisyListener.NO_OP
         try {
-            context.unregisterReceiver(this)
+            appContext.unregisterReceiver(this)
         } catch (ignore: Exception) {
             // ignore if already unregistered
         }
