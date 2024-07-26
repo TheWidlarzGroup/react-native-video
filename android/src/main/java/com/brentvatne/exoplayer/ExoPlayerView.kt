35979a18cdf96942c96675dcd4d6eac432ded04f
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-07-27 00:39:40 +0900

    Migrate ExoPlayerView to Kotlin

--- android/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.kt
+++ android/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.kt
@@ -1,181 +1,174 @@
-package com.brentvatne.exoplayer;
+package com.brentvatne.exoplayer
 
-import android.annotation.SuppressLint;
-import android.content.Context;
+import android.content.Context
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.SurfaceView
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
+import androidx.media3.common.AdViewProvider
+import androidx.media3.common.C
+import androidx.media3.common.Player
+import androidx.media3.common.Tracks
+import androidx.media3.common.VideoSize
+import androidx.media3.common.text.Cue
+import androidx.media3.common.util.Assertions
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.SubtitleView
+import com.brentvatne.common.api.ResizeMode
+import com.brentvatne.common.api.SubtitleStyle
+import com.brentvatne.common.api.ViewType
+import com.brentvatne.common.toolbox.DebugLog
+import com.google.common.collect.ImmutableList
 
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
-import androidx.media3.common.AdViewProvider;
-import androidx.media3.common.C;
-import androidx.media3.common.Format;
-import androidx.media3.common.Player;
-import androidx.media3.common.Tracks;
-import androidx.media3.common.VideoSize;
-import androidx.media3.common.text.Cue;
-import androidx.media3.common.util.Assertions;
-import androidx.media3.exoplayer.ExoPlayer;
-import androidx.media3.ui.SubtitleView;
+@UnstableApi
+class ExoPlayerView(
+    private val context: Context
+) : FrameLayout(context, null, 0), AdViewProvider {
 
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.SurfaceView;
-import android.view.TextureView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
+    private var surfaceView: View? = null
+    private var shutterView: View
+    private var subtitleLayout: SubtitleView
+    private var layout: AspectRatioFrameLayout
+    private var componentListener: ComponentListener
+    private var player: ExoPlayer? = null
+    private var layoutParams: ViewGroup.LayoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+    private var adOverlayFrameLayout: FrameLayout
 
-import com.brentvatne.common.api.ResizeMode;
-import com.brentvatne.common.api.SubtitleStyle;
-import com.brentvatne.common.api.ViewType;
-import com.brentvatne.common.toolbox.DebugLog;
-import com.google.common.collect.ImmutableList;
+    @ViewType.ViewType
+    private var viewType = ViewType.VIEW_TYPE_SURFACE
+    private var hideShutterView = false
 
-import java.util.List;
+    init {
+        componentListener = ComponentListener()
 
-@SuppressLint("ViewConstructor")
-public final class ExoPlayerView extends FrameLayout implements AdViewProvider {
-    private final static String TAG = "ExoPlayerView";
-    private View surfaceView;
-    private final View shutterView;
-    private final SubtitleView subtitleLayout;
-    private final AspectRatioFrameLayout layout;
-    private final ComponentListener componentListener;
-    private ExoPlayer player;
-    private final Context context;
-    private final ViewGroup.LayoutParams layoutParams;
-    private final FrameLayout adOverlayFrameLayout;
+        val aspectRatioParams = LayoutParams(
+            LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT
+        )
+        aspectRatioParams.gravity = Gravity.CENTER
+        layout = AspectRatioFrameLayout(context)
+        layout.layoutParams = aspectRatioParams
 
-    private @ViewType.ViewType int viewType = ViewType.VIEW_TYPE_SURFACE;
-    private boolean hideShutterView = false;
+        shutterView = View(context)
+        shutterView.layoutParams = layoutParams
+        shutterView.setBackgroundColor(ContextCompat.getColor(context, android.R.color.black))
 
-    public ExoPlayerView(Context context) {
-        super(context, null, 0);
+        subtitleLayout = SubtitleView(context)
+        subtitleLayout.layoutParams = layoutParams
+        subtitleLayout.setUserDefaultStyle()
+        subtitleLayout.setUserDefaultTextSize()
 
-        this.context = context;
+        updateSurfaceView(viewType)
 
-        layoutParams = new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT);
+        adOverlayFrameLayout = FrameLayout(context)
 
-        componentListener = new ComponentListener();
+        layout.addView(shutterView, 1, layoutParams)
+        layout.addView(adOverlayFrameLayout, 2, layoutParams)
 
-        FrameLayout.LayoutParams aspectRatioParams = new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.MATCH_PARENT,
-                FrameLayout.LayoutParams.MATCH_PARENT);
-        aspectRatioParams.gravity = Gravity.CENTER;
-        layout = new AspectRatioFrameLayout(context);
-        layout.setLayoutParams(aspectRatioParams);
+        addViewInLayout(layout, 0, aspectRatioParams)
+        addViewInLayout(subtitleLayout, 1, layoutParams)
 
-        shutterView = new View(getContext());
-        shutterView.setLayoutParams(layoutParams);
-        shutterView.setBackgroundColor(ContextCompat.getColor(context, android.R.color.black));
-
-        subtitleLayout = new SubtitleView(context);
-        subtitleLayout.setLayoutParams(layoutParams);
-        subtitleLayout.setUserDefaultStyle();
-        subtitleLayout.setUserDefaultTextSize();
-
-        updateSurfaceView(viewType);
-
-        adOverlayFrameLayout = new FrameLayout(context);
-
-        layout.addView(shutterView, 1, layoutParams);
-        layout.addView(adOverlayFrameLayout, 2, layoutParams);
-
-        addViewInLayout(layout, 0, aspectRatioParams);
-        addViewInLayout(subtitleLayout, 1, layoutParams);
     }
 
-    private void clearVideoView() {
-        if (surfaceView instanceof TextureView) {
-            player.clearVideoTextureView((TextureView) surfaceView);
-        } else if (surfaceView instanceof SurfaceView) {
-            player.clearVideoSurfaceView((SurfaceView) surfaceView);
+    private fun clearVideoView() {
+        if (surfaceView is TextureView) {
+            player?.clearVideoTextureView(surfaceView as TextureView)
+        } else if (surfaceView is SurfaceView) {
+            player?.clearVideoSurfaceView(surfaceView as SurfaceView)
         }
     }
 
-    private void setVideoView() {
-        if (surfaceView instanceof TextureView) {
-            player.setVideoTextureView((TextureView) surfaceView);
-        } else if (surfaceView instanceof SurfaceView) {
-            player.setVideoSurfaceView((SurfaceView) surfaceView);
+    private fun setVideoView() {
+        if (surfaceView is TextureView) {
+            player?.setVideoTextureView(surfaceView as TextureView)
+        } else if (surfaceView is SurfaceView) {
+            player?.setVideoSurfaceView(surfaceView as SurfaceView)
         }
     }
 
-    public boolean isPlaying() {
-        return player != null && player.isPlaying();
+    fun isPlaying(): Boolean {
+        return player != null && player?.isPlaying == true
     }
 
-    public void setSubtitleStyle(SubtitleStyle style) {
-        // ensure we reset subtile style before reapplying it
-        subtitleLayout.setUserDefaultStyle();
-        subtitleLayout.setUserDefaultTextSize();
+    fun setSubtitleStyle(style: SubtitleStyle) {
+        // ensure we reset subtitle style before reapplying it
+        subtitleLayout.setUserDefaultStyle()
+        subtitleLayout.setUserDefaultTextSize()
 
-        if (style.getFontSize() > 0) {
-            subtitleLayout.setFixedTextSize(TypedValue.COMPLEX_UNIT_SP, style.getFontSize());
+        if (style.fontSize > 0) {
+            subtitleLayout.setFixedTextSize(TypedValue.COMPLEX_UNIT_SP, style.fontSize.toFloat())
         }
-        subtitleLayout.setPadding(style.getPaddingLeft(), style.getPaddingTop(), style.getPaddingRight(), style.getPaddingBottom());
-        if (style.getOpacity() != 0) {
-            subtitleLayout.setAlpha(style.getOpacity());
-            subtitleLayout.setVisibility(View.VISIBLE);
+        subtitleLayout.setPadding(style.paddingLeft, style.paddingTop, style.paddingTop, style.paddingBottom)
+        if (style.opacity.toInt() != 0) {
+            subtitleLayout.alpha = style.opacity
+            subtitleLayout.visibility = View.VISIBLE
         } else {
-            subtitleLayout.setVisibility(View.GONE);
+            subtitleLayout.visibility = View.GONE
         }
 
     }
 
-    public void setShutterColor(Integer color) {
-        shutterView.setBackgroundColor(color);
+    fun setShutterColor(color: Int) {
+        shutterView.setBackgroundColor(color)
     }
 
-    public void updateSurfaceView(@ViewType.ViewType int viewType) {
-        this.viewType = viewType;
-        boolean viewNeedRefresh = false;
-        if (viewType == ViewType.VIEW_TYPE_SURFACE || viewType == ViewType.VIEW_TYPE_SURFACE_SECURE) {
-            if (!(surfaceView instanceof SurfaceView)) {
-                surfaceView = new SurfaceView(context);
-                viewNeedRefresh = true;
+    fun updateSurfaceView(@ViewType.ViewType viewType: Int) {
+        this.viewType = viewType
+        var viewNeedRefresh = false
+        when (viewType) {
+            ViewType.VIEW_TYPE_SURFACE, ViewType.VIEW_TYPE_SURFACE_SECURE -> {
+                if (surfaceView !is SurfaceView) {
+                    surfaceView = SurfaceView(context)
+                    viewNeedRefresh = true
+                }
+                (surfaceView as SurfaceView).setSecure(viewType == ViewType.VIEW_TYPE_SURFACE_SECURE)
             }
-            ((SurfaceView)surfaceView).setSecure(viewType == ViewType.VIEW_TYPE_SURFACE_SECURE);
-        } else if (viewType == ViewType.VIEW_TYPE_TEXTURE) {
-            if (!(surfaceView instanceof TextureView)) {
-                surfaceView = new TextureView(context);
-                viewNeedRefresh = true;
+            ViewType.VIEW_TYPE_TEXTURE -> {
+                if (surfaceView !is TextureView) {
+                    surfaceView = TextureView(context)
+                    viewNeedRefresh = true
+                }
+                // Support opacity properly:
+                (surfaceView as TextureView).isOpaque = false
             }
-            // Support opacity properly:
-            ((TextureView) surfaceView).setOpaque(false);
-        } else {
-            DebugLog.wtf(TAG, "wtf is this texture " + viewType);
+            else -> {
+                DebugLog.wtf(TAG, "wtf is this texture $viewType")
+            }
         }
         if (viewNeedRefresh) {
-            surfaceView.setLayoutParams(layoutParams);
+            surfaceView?.layoutParams = layoutParams
 
             if (layout.getChildAt(0) != null) {
-                layout.removeViewAt(0);
+                layout.removeViewAt(0)
             }
-            layout.addView(surfaceView, 0, layoutParams);
+            layout.addView(surfaceView, 0, layoutParams)
 
             if (this.player != null) {
-                setVideoView();
+                setVideoView()
             }
         }
     }
 
-    private void updateShutterViewVisibility() {
-        shutterView.setVisibility(this.hideShutterView ? View.INVISIBLE : View.VISIBLE);
+    private fun updateShutterViewVisibility() {
+        shutterView.visibility = if (this.hideShutterView) {
+            View.INVISIBLE
+        } else {
+            View.VISIBLE
+        }
     }
 
-    @Override
-    public void requestLayout() {
-        super.requestLayout();
-        post(measureAndLayout);
+    override fun requestLayout() {
+        super.requestLayout()
+        post(measureAndLayout)
     }
 
     // AdsLoader.AdViewProvider implementation.
-
-    @Override
-    public ViewGroup getAdViewGroup() {
-        return Assertions.checkNotNull(adOverlayFrameLayout, "exo_ad_overlay must be present for ad playback");
+    override fun getAdViewGroup(): ViewGroup {
+        return Assertions.checkNotNull(adOverlayFrameLayout, "exo_ad_overlay must be present for ad playback")
     }
 
     /**
@@ -185,19 +178,23 @@ public final class ExoPlayerView extends FrameLayout implements AdViewProvider {
      *
      * @param player The {@link ExoPlayer} to use.
      */
-    public void setPlayer(ExoPlayer player) {
+    fun setPlayer(player: ExoPlayer?) {
         if (this.player == player) {
-            return;
+            return
         }
         if (this.player != null) {
-            this.player.removeListener(componentListener);
-            clearVideoView();
+            this.player!!.removeListener(componentListener)
+            clearVideoView()
         }
-        this.player = player;
-        shutterView.setVisibility(this.hideShutterView ? View.INVISIBLE : View.VISIBLE);
+        this.player = player
+        shutterView.visibility = if (this.hideShutterView) {
+            View.INVISIBLE
+        } else {
+            View.VISIBLE
+        }
         if (player != null) {
-            setVideoView();
-            player.addListener(componentListener);
+            setVideoView()
+            player.addListener(componentListener)
         }
     }
 
@@ -206,87 +203,89 @@ public final class ExoPlayerView extends FrameLayout implements AdViewProvider {
      *
      * @param resizeMode The resize mode.
      */
-    public void setResizeMode(@ResizeMode.Mode int resizeMode) {
-        if (layout != null && layout.getResizeMode() != resizeMode) {
-            layout.setResizeMode(resizeMode);
-            post(measureAndLayout);
+    fun setResizeMode(@ResizeMode.Mode resizeMode: Int) {
+        if (layout.resizeMode != resizeMode) {
+            layout.resizeMode = resizeMode
+            post(measureAndLayout)
         }
     }
 
-    public void setHideShutterView(boolean hideShutterView) {
-        this.hideShutterView = hideShutterView;
-        updateShutterViewVisibility();
+    fun setHideShutterView(hideShutterView: Boolean) {
+        this.hideShutterView = hideShutterView
+        updateShutterViewVisibility()
     }
 
-    private final Runnable measureAndLayout = () -> {
+    private val measureAndLayout: Runnable = Runnable {
         measure(
-                MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
-                MeasureSpec.makeMeasureSpec(getHeight(), MeasureSpec.EXACTLY));
-        layout(getLeft(), getTop(), getRight(), getBottom());
-    };
+            MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY)
+        )
+        layout(left, top, right, bottom)
+    }
 
-    private void updateForCurrentTrackSelections(Tracks tracks) {
+    private fun updateForCurrentTrackSelections(tracks: Tracks?) {
         if (tracks == null) {
-            return;
+            return
         }
-        ImmutableList<Tracks.Group> groups = tracks.getGroups();
-        for (Tracks.Group group: groups) {
-            if (group.getType() == C.TRACK_TYPE_VIDEO && group.length > 0) {
-                // get the first track of the group to identify aspect ratio
-                Format format = group.getTrackFormat(0);
+        val groups: ImmutableList<Tracks.Group> = tracks.groups
 
-                // There are weird cases when video height and width did not change with rotation so we need change aspect ration to fix it
-                switch (format.rotationDegrees) {
+        for (group in groups) {
+            if (group.type == C.TRACK_TYPE_VIDEO && group.length > 0) {
+                // get the first track of the group to identify aspect ratio
+                val format = group.getTrackFormat(0)
+
+                // There are weird cases when video height and width did not change with rotation so we need change aspect ration to fix
+                layout.videoAspectRatio = when (format.rotationDegrees) {
                     // update aspect ratio !
-                    case 90:
-                    case 270:
-                        layout.setVideoAspectRatio(format.width == 0 ? 1 : (format.height * format.pixelWidthHeightRatio) / format.width);
-                    default:
-                        layout.setVideoAspectRatio(format.height == 0 ? 1 : (format.width * format.pixelWidthHeightRatio) / format.height);
+                    90, 270 -> (format.height * format.pixelWidthHeightRatio / format.width)
+                    else -> (format.width * format.pixelWidthHeightRatio / format.height)
                 }
-                return;
+                return
             }
         }
         // no video tracks, in that case refresh shutterView visibility
-        shutterView.setVisibility(this.hideShutterView ? View.INVISIBLE : View.VISIBLE);
+        shutterView.visibility = if (this.hideShutterView) {
+            View.INVISIBLE
+        } else {
+            View.VISIBLE
+        }
     }
 
-    public void invalidateAspectRatio() {
+    fun invalidateAspectRatio() {
         // Resetting aspect ratio will force layout refresh on next video size changed
-        layout.invalidateAspectRatio();
+        layout.invalidateAspectRatio()
     }
 
-    private final class ComponentListener implements Player.Listener {
+    private inner class ComponentListener : Player.Listener {
 
-        @Override
-        public void onCues(@NonNull List<Cue> cues) {
-            subtitleLayout.setCues(cues);
+        override fun onCues(cues: List<Cue>) {
+            subtitleLayout.setCues(cues)
         }
 
-        @Override
-        public void onVideoSizeChanged(VideoSize videoSize) {
-            boolean isInitialRatio = layout.getVideoAspectRatio() == 0;
+        override fun onVideoSizeChanged(videoSize: VideoSize) {
+            val isInitialRatio = layout.videoAspectRatio == 0f
             if (videoSize.height == 0 || videoSize.width == 0) {
                 // When changing video track we receive an ghost state with height / width = 0
                 // No need to resize the view in that case
-                return;
+                return
             }
-            layout.setVideoAspectRatio((videoSize.width * videoSize.pixelWidthHeightRatio) / videoSize.height);
+            layout.videoAspectRatio = ((videoSize.width * videoSize.pixelWidthHeightRatio) / videoSize.height)
 
             // React native workaround for measuring and layout on initial load.
             if (isInitialRatio) {
-                post(measureAndLayout);
+                post(measureAndLayout)
             }
         }
 
-        @Override
-        public void onRenderedFirstFrame() {
-            shutterView.setVisibility(INVISIBLE);
+        override fun onRenderedFirstFrame() {
+            shutterView.visibility = INVISIBLE
         }
 
-        @Override
-        public void onTracksChanged(@NonNull Tracks tracks) {
-            updateForCurrentTrackSelections(tracks);
+        override fun onTracksChanged(tracks: Tracks) {
+            updateForCurrentTrackSelections(tracks)
         }
+    }
+
+    companion object {
+        private const val TAG = "ExoPlayerView"
     }
 }
