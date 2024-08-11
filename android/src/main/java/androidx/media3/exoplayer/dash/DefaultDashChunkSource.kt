64c833f81979e44824a17ac2169298e4a1dd03ce
mostafahasani <seyedmostafahassani@gmail.com>
2024-08-11 23:19:46 +0330

    refactor(android): migrate DefaultDashChunkSource to Kotlin

--- android/src/main/java/androidx/media3/exoplayer/dash/DefaultDashChunkSource.kt
+++ android/src/main/java/androidx/media3/exoplayer/dash/DefaultDashChunkSource.kt
@@ -1,10 +1,7 @@
-package androidx.media3.exoplayer.dash;
+package androidx.media3.exoplayer.dash
 
-import androidx.media3.datasource.DataSource;
+import androidx.media3.datasource.DataSource
 
-public class DefaultDashChunkSource {
-    public static final class Factory {
-        public Factory(DataSource.Factory mediaDataSourceFactory) {
-        }
-    }
+class DefaultDashChunkSource {
+    class Factory(private val mediaDataSourceFactory: DataSource.Factory)
 }
