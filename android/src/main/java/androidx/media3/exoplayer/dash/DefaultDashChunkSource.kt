a511e3665ebde01401fb8ddcbb52d4c1a5e12a8a
Seyed Mostafa Hasani <seyedmostafahassani@gmail.com>
2024-08-17 12:15:45 +0330

    fix(android): resolve a release issue with DefaultDashChunkSource

--- android/src/main/java/androidx/media3/exoplayer/dash/DefaultDashChunkSource.kt
+++
@@ -1,7 +0,0 @@
-package androidx.media3.exoplayer.dash
-
-import androidx.media3.datasource.DataSource
-
-class DefaultDashChunkSource {
-    class Factory(private val mediaDataSourceFactory: DataSource.Factory)
-}
