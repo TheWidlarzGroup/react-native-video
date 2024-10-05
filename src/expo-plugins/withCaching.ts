9fe39af18a89b57bf3a7e2b2649404fc8e6e7d8c
Krzysztof Moch <krzysmoch.programs@gmail.com>
2024-10-05 13:17:15 +0200

    add test app suport in expo plugin

--- src/expo-plugins/withCaching.ts
+++ src/expo-plugins/withCaching.ts
@@ -4,10 +4,10 @@ import {writeToPodfile} from './writeToPodfile';
 /**
  * Sets whether to include the cache dependency to use cache on iOS with `react-native-video`.
  */
-export const withCaching: ConfigPlugin<boolean> = (
-  c,
-  enableCachingExtension,
-) => {
+export const withCaching: ConfigPlugin<{
+  enableCachingExtension: boolean;
+  testApp?: boolean;
+}> = (c, {enableCachingExtension, testApp = false}) => {
   const ios_key = 'RNVideoUseVideoCaching';
 
   return withDangerousMod(c, [
@@ -17,6 +17,7 @@ export const withCaching: ConfigPlugin<boolean> = (
         config.modRequest.projectRoot,
         ios_key,
         enableCachingExtension.toString(),
+        testApp,
       );
       return config;
     },
