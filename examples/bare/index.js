3d6c235e9c66352eb8e4816e9c215f31de64916c
Krzysztof Moch <krzysmoch.programs@gmail.com>
2024-10-05 18:06:18 +0200

    add src files

--- examples/bare/index.js
+++ examples/bare/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import BasicExample from './src/BasicExample';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => BasicExample);
---
+++ examples/bare/src/assets/videos/index.ts
@@ -0,0 +1,4 @@
+export const localeVideo = {
+  broadchurch: require('./broadchurch.mp4'),
+  portrait: require('./portrait.mp4'),
+};
