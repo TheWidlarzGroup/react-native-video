d7b52141625042db4c77321db10642512f2708b0
Krzysztof Moch <krzysmoch.programs@gmail.com>
2024-10-08 16:51:25 +0200

    remove old examples

--- examples/basic/src/assets/videos/index.ts
+++
@@ -1,4 +0,0 @@
-export const localeVideo = {
-  broadchurch: require('./broadchurch.mp4'),
-  portrait: require('./portrait.mp4'),
-};
--- examples/basic/src/components/Indicator.tsx
+++
@@ -1,8 +0,0 @@
-import React, {memo} from 'react';
-import {ActivityIndicator} from 'react-native';
-
-const _Indicator = () => {
-  return <ActivityIndicator color="#3235fd" size="large" />;
-};
-
-export const Indicator = memo(_Indicator);
