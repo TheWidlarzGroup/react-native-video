4464929e4d77398c09bb7a98c188dbd76167934f
Krzysztof Moch <krzysmoch.programs@gmail.com>
2024-10-23 12:22:24 +0200

    move library code to package and use bun

--- packages/react-native-video-plugin-sample/src/index.tsx
+++ packages/react-native-video-plugin-sample/src/index.tsx
@@ -1,8 +1,8 @@
-import { NativeModules, Platform } from 'react-native';
+import {NativeModules, Platform} from 'react-native';
 
 const LINKING_ERROR =
-  `The package 'react-native-video-plugin-sample' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  "The package 'react-native-video-plugin-sample' doesn't seem to be linked. Make sure: \n\n" +
+  Platform.select({ios: "- You have run 'pod install'\n", default: ''}) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
@@ -14,7 +14,7 @@ const VideoPluginSample = NativeModules.VideoPluginSample
         get() {
           throw new Error(LINKING_ERROR);
         },
-      }
+      },
     );
 
 export function multiply(a: number, b: number): Promise<number> {
