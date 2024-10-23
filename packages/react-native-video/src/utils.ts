afc6353a53232254b5caca59142899fc25fe600c
Krzysztof Moch <krzysmoch.programs@gmail.com>
2024-10-23 13:13:38 +0200

    fix typescript react resolution

--- packages/react-native-video/src/utils.ts
+++ packages/react-native-video/src/utils.ts
@@ -58,7 +58,6 @@ export function getReactTag(
     throw new Error('Video Component is not mounted');
   }
 
-  // @ts-expect-error Not sure how to satisfy Typescript here
   const reactTag = findNodeHandle(ref.current);
 
   if (!reactTag) {
