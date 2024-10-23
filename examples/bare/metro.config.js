dc2a5af99eabc37e120b2c7c8e5c926d012f5ee0
Krzysztof Moch <krzysmoch.programs@gmail.com>
2024-10-23 13:53:54 +0200

    update bare example config

--- examples/bare/metro.config.js
+++ examples/bare/metro.config.js
@@ -1,5 +1,12 @@
 const path = require('path');
+const escape = require('escape-string-regexp');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 const {makeMetroConfig} = require('@rnx-kit/metro-config');
+
+const pak = require('../../package.json');
+
+const root = path.resolve(__dirname, '../..');
+const modules = Object.keys({...pak.peerDependencies});
 
 module.exports = makeMetroConfig({
   transformer: {
@@ -11,10 +18,21 @@ module.exports = makeMetroConfig({
     }),
   },
   resolver: {
-    enableSymlinks: true,
+    // We need to make sure that only one version is loaded for peerDependencies
+    // So we block them at the root, and alias them to the versions in example's node_modules
+    resolver: {
+      blacklistRE: exclusionList(
+        modules.map(
+          (m) =>
+            new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
+        ),
+      ),
+
+      extraNodeModules: modules.reduce((acc, name) => {
+        acc[name] = path.join(__dirname, 'node_modules', name);
+        return acc;
+      }, {}),
+    },
   },
-  watchFolders: [
-    path.join(__dirname, 'node_modules', 'react-native-video'),
-    path.resolve(__dirname, '../..'),
-  ],
+  watchFolders: [root],
 });
