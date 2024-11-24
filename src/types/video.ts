2d9a39b135e3db5f1205d2e876b509e9ee3b892f
Olivier Bouillet <freeboub@gmail.com>
2024-11-24 22:35:50 +0100

    fix(android): disable caching on local asset files

--- src/types/video.ts
+++ src/types/video.ts
@@ -24,6 +24,7 @@ export type ReactVideoSourceProperties = {
   uri?: string;
   isNetwork?: boolean;
   isAsset?: boolean;
+  isLocalAssetFile?: boolean;
   shouldCache?: boolean;
   type?: string;
   mainVer?: number;
