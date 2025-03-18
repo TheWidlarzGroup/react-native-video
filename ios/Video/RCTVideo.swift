1142102f30e79f1f8485c621c74e4c577f1fdad1
Wojciech Ogrodowczyk <wojciech@brainsandbeards.com>
2025-03-18 09:48:02 +0100

    Don't try to modify react view after unmount

--- ios/Video/RCTVideo.swift
+++ ios/Video/RCTVideo.swift
@@ -1679,8 +1679,10 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
                 }
             }
 
-            self.reactViewController().view.frame = bounds
-            self.reactViewController().view.setNeedsLayout()
+            if let reactVC = self.reactViewController() {
+                reactVC.view.frame = bounds
+                reactVC.view.setNeedsLayout()
+            }
         }
     }
 
