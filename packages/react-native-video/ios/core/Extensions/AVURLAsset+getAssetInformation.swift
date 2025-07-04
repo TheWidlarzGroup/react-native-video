01d151bdde1f97f063ba5e4b70d9e0949369e6bb
Krzysztof Moch <krzysmoch.programs@gmail.com>
2025-07-04 10:59:05 +0200

    fix(ios): `getAssetInformationAsync` does not provide the video height/width for hls streams

--- packages/react-native-video/ios/core/Extensions/AVURLAsset+getAssetInformation.swift
+++ packages/react-native-video/ios/core/Extensions/AVURLAsset+getAssetInformation.swift
@@ -13,9 +13,9 @@ extension AVURLAsset {
       isLive: false,
       orientation: .unknown
     )
-    
+
     videoInformation.fileSize = try await VideoFileHelper.getFileSize(for: url)
-    
+
     // Check if asset is live stream
     if duration.flags.contains(.indefinite) {
       videoInformation.duration = -1
@@ -24,21 +24,45 @@ extension AVURLAsset {
       videoInformation.duration = Int64(CMTimeGetSeconds(duration))
       videoInformation.isLive = false
     }
-    
+
     if let videoTrack = tracks(withMediaType: .video).first {
       let size = videoTrack.naturalSize.applying(videoTrack.preferredTransform)
       videoInformation.width = size.width
       videoInformation.height = size.height
-      
+
       videoInformation.bitrate = Double(videoTrack.estimatedDataRate)
-      
+
       videoInformation.orientation = videoTrack.orientation
-      
+
       if #available(iOS 14.0, tvOS 14.0, visionOS 1.0, *) {
         videoInformation.isHDR = videoTrack.hasMediaCharacteristic(.containsHDRVideo)
       }
+    } else if url.pathExtension == "m3u8" {
+      // For HLS streams, we cannot get video track information directly
+      // So we download manifest and try to extract video information from it
+
+      let manifestContent = try await HLSManifestParser.downloadManifest(from: url)
+      let manifestInfo = try HLSManifestParser.parseM3U8Manifest(manifestContent)
+
+      if let videoStream = manifestInfo.streams.first {
+        videoInformation.width = Double(videoStream.width ?? Int(Double.nan))
+        videoInformation.height = Double(videoStream.height ?? Int(Double.nan))
+        videoInformation.bitrate = Double(videoStream.bandwidth ?? Int(Double.nan))
+      }
+
+      if videoInformation.width > 0 && videoInformation.height > 0 {
+        if videoInformation.width == videoInformation.height {
+          videoInformation.orientation = .square
+        } else if videoInformation.width > videoInformation.height {
+          videoInformation.orientation = .landscapeRight
+        } else if videoInformation.width < videoInformation.height {
+          videoInformation.orientation = .portrait
+        } else {
+          videoInformation.orientation = .unknown
+        }
+      }
     }
-    
+
     return videoInformation
   }
 }
