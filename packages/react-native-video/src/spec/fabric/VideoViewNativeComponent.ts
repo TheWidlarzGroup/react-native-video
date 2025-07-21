7b8d0b879dabc0a7b2beeae1fd5b24e208b8e07f
Krzysztof Moch <krzysmoch.programs@gmail.com>
2025-07-21 11:54:37 +0200

    fix: codegen config (#4616)

--- packages/react-native-video/src/spec/fabric/VideoViewNativeComponent.ts
+++ packages/react-native-video/src/spec/fabric/VideoViewNativeComponent.ts
@@ -14,4 +14,4 @@ export interface ViewViewNativeProps extends ViewProps {
   onNitroIdChange?: DirectEventHandler<OnNitroIdChangeEvent>;
 }
 
-export default codegenNativeComponent<ViewViewNativeProps>('VideoView');
+export default codegenNativeComponent<ViewViewNativeProps>('RNCVideoView');
