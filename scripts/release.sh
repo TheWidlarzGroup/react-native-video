52499e5af795a4703e17741ac348b40226ad6729
Krzysztof Moch <krzysmoch.programs@gmail.com>
2025-10-10 15:57:09 +0200

    chore: release 7.0.0-alpha.6

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,18 +11,33 @@ if [ ! -d "packages/react-native-video" ]; then
   exit 1
 fi
 
-echo "[React Native Video] Publishing main package"
-cd packages/react-native-video
-bun run release $@
-cd ../..
+# Check if options include --skip react-native-video
+if [[ " $@ " =~ " --skip " ]] && [[ " $@ " =~ " react-native-video " ]]; then
+  echo "[React Native Video] Skipping main package release"
+else
+  echo "[React Native Video] Publishing main package"
+  cd packages/react-native-video
+  bun run release $@
+  cd ../..
+fi
 
-echo "[DRM Plugin] Publishing drm plugin"
+# Check if options include --skip drm-plugin
+if [[ " $@ " =~ " --skip " ]] && [[ " $@ " =~ " drm-plugin " ]]; then
+  echo "[DRM Plugin] Skipping drm plugin release"
+else
+  echo "[DRM Plugin] Publishing drm plugin"
 
-cd packages/drm-plugin
-bun run release $@
-cd ../..
+  cd packages/drm-plugin
+  bun run release $@
+  cd ../..
+fi
 
-echo "[React Native Video] Making Github Release"
-bun run release:github $@
+# Check if options include --skip github
+if [[ " $@ " =~ " --skip " ]] && [[ " $@ " =~ " github " ]]; then
+  echo "[React Native Video] Skipping GitHub release"
+else
+  echo "[React Native Video] Making Github Release"
+  bun run release:github $@
+fi
 
 echo "[React Native Video] Done"
