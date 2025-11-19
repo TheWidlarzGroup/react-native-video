e3c15f9c1367bcf7759e2e7b458e01adf04ee9e6
Kamil Moskała <91079590+moskalakamil@users.noreply.github.com>
2025-11-19 18:23:34 +0100

    refactor: remove redundant frame initialization

--- ios/Video/Features/RCTIMAAdsManager.swift
+++ ios/Video/Features/RCTIMAAdsManager.swift
@@ -31,7 +31,7 @@
         func requestAds() {
             guard let _video else { return }
             // fixes RCTVideo --> RCTIMAAdsManager --> IMAAdsLoader --> IMAAdDisplayContainer --> RCTVideo memory leak.
-            let adContainerView = UIView(frame: _video.bounds)
+            let adContainerView = UIView()
             adContainerView.backgroundColor = .clear
             _video.addSubview(adContainerView)
 
